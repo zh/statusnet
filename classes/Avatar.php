c87951f89518e276a85f41eaecf34da0720f49c5
Evan Prodromou <evan@prodromou.name>
2008-05-19 09:40:57 -0400

    update avatar table
    darcs-hash:20080519134057-84dde-d30a178de9e3e7d6ced21e9c395dfac545b3d343.gz

--- classes/Avatar.php
+++ classes/Avatar.php
@@ -11,10 +11,14 @@ class Avatar extends DB_DataObject
 
     public $__table = 'avatar';                          // table name
     public $profile_id;                      // int(4)  primary_key not_null
+    public $original;                        // tinyint(1)  
     public $width;                           // int(4)  primary_key not_null
     public $height;                          // int(4)  primary_key not_null
-    public $original;                        // tinyint(1)  
     public $mediatype;                       // varchar(32)   not_null
+    public $filename;                        // varchar(255)  
+    public $url;                             // varchar(255)  unique_key
+    public $created;                         // datetime()   not_null
+    public $modified;                        // timestamp()   not_null default_CURRENT_TIMESTAMP
 
     /* Static get */
     function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('Avatar',$k,$v); }
