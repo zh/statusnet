7b505743f2b5adf0aeeaaf1faa116f3d86752d82
Evan Prodromou <evan@prodromou.name>
2008-05-28 13:17:58 -0400

    add span around license link
    darcs-hash:20080528171758-84dde-6121afedbce9bbecea09386e347aa3d1c85c8f44.gz

--- lib/util.php
+++ lib/util.php
@@ -153,13 +153,15 @@ function common_text($txt) {
 function common_license_block() {
 	global $config, $xw;
 	common_element_start('p', 'license');
-	common_element_start('a', array('class' => 'license floatLeft width25',
+	common_element_start('span', 'floatLeft width25');
+	common_element_start('a', array('class' => 'license',
 									'rel' => 'license',
 									href => $config['license']['url']));
 	common_element('img', array('class' => 'license',
 								'src' => $config['license']['image'],
 								'alt' => $config['license']['title']));
 	common_element_end('a');
+	common_element_end('span');
 	common_element_start('span', 'floatRight width75');
 	common_text(_t('Unless otherwise specified, contents of this site are copyright by the contributors and available under the '));
 	common_element('a', array('class' => 'license',
