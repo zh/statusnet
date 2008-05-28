dc37b88e543149827cc141b0c44fc6e162c655ff
Evan Prodromou <evan@prodromou.name>
2008-05-28 13:31:29 -0400

    paginate on showstream
    darcs-hash:20080528173129-84dde-8f0537b20173afa895aeb76e503d76cc24fdae41.gz

--- actions/showstream.php
+++ actions/showstream.php
@@ -263,19 +263,44 @@ class ShowstreamAction extends StreamAction {
 
 		$page = $this->arg('page') || 1;
 
-		$notice->limit((($page-1)*NOTICES_PER_PAGE), NOTICES_PER_PAGE);
+		$notice->limit((($page-1)*NOTICES_PER_PAGE), NOTICES_PER_PAGE + 1);
 
-		$notice->find();
+		$cnt = $notice->find();
 
 		common_element_start('div', 'notices width66 floatLeft');
 
 		common_element_start('ul', 'bigLinks');
-		
-		while ($notice->fetch()) {
-			$this->show_notice($notice);
+
+		for ($i = 0; $i < min($cnt, NOTICES_PER_PAGE); $i++) {
+			if ($notice->fetch()) {
+				$this->show_notice($notice);
+			} else {
+				// shouldn't happen!
+				break;
+			}
 		}
 		
 		common_element_end('ul');
+
+		if ($page > 1) {
+			common_element_start('span', 'floatLeft width25');
+			common_element('a', array('href' => common_local_url('showstream', 
+																 array('nickname' => $profile->nickname,
+																	   'page' => $page-1))
+									  'class' => 'newer')
+						   _t('Newer'));
+			common_element_end('span');
+		}
+		
+		if ($cnt > NOTICES_PER_PAGE) {
+			common_element_start('span', 'floatRight width25');
+			common_element('a', array('href' => common_local_url('showstream', 
+																 array('nickname' => $profile->nickname,
+																	   'page' => $page+1))
+									  'class' => 'older')
+						   _t('Older'));
+			common_element_end('span');
+		}
 		
 		# XXX: show a link for the next page
 		common_element_end('div');
