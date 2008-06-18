5f0bf3d88ea8a3cd31dc8f6e042006468f757e4e
Evan Prodromou <evan@prodromou.name>
2008-06-18 07:01:26 -0400

    user_id in user_openid is not unique
    darcs-hash:20080618110126-84dde-4dd97a6e08fb95b0f9860edda45ccaedb3958b20.gz

--- classes/User_openid.php
+++ classes/User_openid.php
@@ -12,7 +12,7 @@ class User_openid extends DB_DataObject
     public $__table = 'user_openid';                     // table name
     public $canonical;                       // varchar(255)  primary_key not_null
     public $display;                         // varchar(255)  unique_key not_null
-    public $user_id;                         // int(4)  unique_key not_null
+    public $user_id;                         // int(4)   not_null
     public $created;                         // datetime()   not_null
     public $modified;                        // timestamp()   not_null default_CURRENT_TIMESTAMP
 
