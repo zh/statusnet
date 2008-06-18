79ac3376959e6c22cfd6c6d053752be3d2bb8537
Evan Prodromou <evan@prodromou.name>
2008-06-18 11:30:36 -0400

    more rigorous check on site logo
    darcs-hash:20080618153036-84dde-06329e00661924826d6fd2b5b0682957c21c06d9.gz

--- lib/util.php
+++ lib/util.php
@@ -183,7 +183,9 @@ function common_show_header($pagetitle, $callable=NULL, $data=NULL, $headercall=
 	common_element_start('div', array('id' => 'wrap'));
 	common_element_start('div', array('id' => 'header'));
 	common_nav_menu();
-	if ($config['site']['logo'] || file_exists(theme_file('logo.png'))) {
+	if ((is_string($config['site']['logo']) && (strlen($config['site']['logo']) > 0))
+		|| file_exists(theme_file('logo.png')))
+	{
 		common_element_start('a', array('href' => common_local_url('public')));
 		common_element('img', array('src' => ($config['site']['logo']) ?
 									($config['site']['logo']) : theme_path('logo.png'),
