8ac3474361a62b58993b4d3a2e23b53aff843532
Evan Prodromou <evan@prodromou.name>
2008-06-22 23:05:23 -0400

    dont let the daemon get called from a web server
    darcs-hash:20080623030523-84dde-d667f0e35b5719cee8496df4b0782dbb4afe5177.gz

--- xmppdaemon.php
+++ xmppdaemon.php
@@ -17,6 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+# Abort if called from a web server
+if (isset($_SERVER) && array_key_exists('REQUEST_METHOD', $_SERVER)) {
+	print "This script must be run from the command line\n";
+	exit();
+}
+
 define('INSTALLDIR', dirname(__FILE__));
 define('LACONICA', true);
 
