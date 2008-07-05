5b377a31988f1f7aa38f33f0010c842d46ee6ff8
Evan Prodromou <evan@prodromou.name>
2008-07-05 12:16:02 -0400

    debugging code for rm
    darcs-hash:20080705161602-84dde-f046e62ed9af829c86ac967f83168933e6264deb.gz

--- actions/register.php
+++ actions/register.php
@@ -70,6 +70,8 @@ class RegisterAction extends Action {
 				return;
 			}
 			common_real_login(true);
+			common_debug('Remember me is "' . $this->arg('rememberme') . '"');
+			common_debug('Remember me value is "' . $this->boolean('rememberme') . '"');
 			if ($this->boolean('rememberme')) {
 				common_rememberme();
 			}
