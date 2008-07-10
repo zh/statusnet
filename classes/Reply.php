98cf96b3e4f642158a13d12d248170367332cce9
Mike Cochrane <mikec@mikenz.geek.nz>
2008-07-10 00:59:42 -0400

    Resolve conflict properly
    darcs-hash:20080710045942-533db-209fce2e2e89dfc809c9b25e93fb428c585f29e4.gz

--- classes/Reply.php
+++ classes/Reply.php
@@ -14,7 +14,6 @@ class Reply extends DB_DataObject
     public $profile_id;                      // int(4)  primary_key not_null
     public $replied_id;                      // int(4)
     public $modified;                        // timestamp()   not_null default_CURRENT_TIMESTAMP
-    public $replied_id;                      // int(4)
 
     /* Static get */
     function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('Reply',$k,$v); }
