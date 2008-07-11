335d5f2e89261cdb74c12ddb9e46d549200d3364
Evan Prodromou <evan@prodromou.name>
2008-07-11 13:11:13 -0400

    add 'avatar' to blacklisted names
    darcs-hash:20080711171113-84dde-56b61d305e207bdb7d1fa844356fbc41e5bd43b9.gz

--- classes/User.php
+++ classes/User.php
@@ -91,7 +91,7 @@ class User extends DB_DataObject
 		# XXX: should already be validated for size, content, etc.
 		static $blacklist = array('rss', 'xrds', 'doc', 'main',
 								  'settings', 'notice', 'user',
-								  'search');
+								  'search', 'avatar');
 		$merged = array_merge($blacklist, common_config('nickname', 'blacklist'));
 		return !in_array($nickname, $merged);
 	}
