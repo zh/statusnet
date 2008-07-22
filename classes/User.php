97b1552c9a02a0f11118685f5d9475941ba81454
Evan Prodromou <evan@prodromou.name>
2008-07-22 12:17:52 -0400

    another whitespace SQL error
    darcs-hash:20080722161752-84dde-aaf5762ef3d376f0e8f335ff8f7811e3162d33fb.gz

--- classes/User.php
+++ classes/User.php
@@ -134,7 +134,7 @@ class User extends DB_DataObject
 		$notice = new Notice();
 		
 		$notice->query('SELECT notice.* ' .
-					   'FROM notice JOIN subscription on notice.profile_id = subscription.subscribed' .
+					   'FROM notice JOIN subscription on notice.profile_id = subscription.subscribed ' .
 					   'WHERE subscription.subscriber = ' . $this->id . ' ' .
 					   'ORDER BY created DESC, notice.id DESC ' .
 					   'LIMIT ' . $offset . ', ' . $limit);
