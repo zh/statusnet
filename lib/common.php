27b7987b3ce527799a6fbe5b2b9eab0b1030eca5
Evan Prodromou <evan@controlyourself.ca>
2008-09-23 00:54:49 -0400

    version number -- doh!
    darcs-hash:20080923045449-5ed1f-073632fdff95f4869730d01d57cfdb83b31401a9.gz

--- lib/common.php
+++ lib/common.php
@@ -19,7 +19,7 @@
 
 if (!defined('LACONICA')) { exit(1); }
 
-define('LACONICA_VERSION', '0.5.0');
+define('LACONICA_VERSION', '0.6.0');
 
 define('AVATAR_PROFILE_SIZE', 96);
 define('AVATAR_STREAM_SIZE', 48);
