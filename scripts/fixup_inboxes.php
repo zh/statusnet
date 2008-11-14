11fba88213f0aa13e9e62d2a77cf3ab4fdc4ac8a
Evan Prodromou <evan@prodromou.name>
2008-11-14 01:51:45 -0500

    don't ignore errors on inbox fixup; it messes up replication
    darcs-hash:20081114065145-84dde-869571e84951470ab40f5b5191d099d23e68b27b.gz

--- scripts/fixup_inboxes.php
+++ scripts/fixup_inboxes.php
@@ -51,7 +51,13 @@ while ($user->fetch()) {
     common_log(LOG_INFO, 'Updating inbox for user ' . $user->id);
 	$user->query('BEGIN');
 	$inbox = new Notice_inbox();
-	$result = $inbox->query('INSERT LOW_PRIORITY IGNORE INTO notice_inbox (user_id, notice_id, created) ' .
+	$inbox->user_id = $user->id;
+	$result = $inbox->delete();
+	if (is_null($result) || $result === false) {
+		common_log_db_error($inbox, 'DELETE', __FILE__);
+		continue;
+	}
+	$result = $inbox->query('INSERT LOW_PRIORITY INTO notice_inbox (user_id, notice_id, created) ' .
 							'SELECT ' . $user->id . ', notice.id, notice.created ' .
 							'FROM subscription JOIN notice ON subscription.subscribed = notice.profile_id ' .
 							'WHERE subscription.subscriber = ' . $user->id . ' ' .
