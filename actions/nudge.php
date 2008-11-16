1b9bab7328a535f69e1480fca30b8b8bf70d5c88
csarven <csarven@controlyourself.ca>
2008-11-15 22:31:55 -0500

    added nudge.php action file
    darcs-hash:20081116033155-eefa4-3fedb1932819520c6b35b2a543f4dcfd5014e872.gz

---
+++ actions/nudge.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Laconica - a distributed open-source microblogging tool
+ * Copyright (C) 2008, Controlez-Vous, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!defined('LACONICA')) { exit(1); }
+
+require_once(INSTALLDIR.'/lib/mail.php');
+
+class NudgeAction extends Action {
+
+	function handle($args) {
+		parent::handle($args);
+
+		if (!common_logged_in()) {
+			common_user_error(_('Not logged in.'));
+			return;
+		}
+
+		$user = common_current_user();
+		$other_nickname = common_canonical_nickname($args['nickname']);
+		$other = User::staticGet('nickname', $other_nickname);
+		$this->notify($user, $other);
+
+		if (true || $this->boolean('ajax')) {
+			common_start_html('text/xml');
+			common_element_start('head');
+			common_element('title', null, _('Nudge sent'));
+			common_element_end('head');
+			common_element_start('body');
+			common_nudge_response();
+			common_element_end('body');
+			common_element_end('html');
+		} else {
+			common_redirect(common_local_url('nudge',
+											 array('nickname' => $user->nickname)));
+		}
+	}
+
+	function notify($user, $other) {
+		if ($other && $other->id != $user->id) {
+			if ($other->email && $other->emailnotifynudge) {
+				mail_notify_nudge($user, $other);
+			}
+			# XXX: notify by IM
+			# XXX: notify by SMS
+		}
+	}
+
+}
