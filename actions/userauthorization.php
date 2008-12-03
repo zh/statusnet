8b3080faf4710ad86fcb56b2b193ac20a47a1c57
Evan Prodromou <evan@controlyourself.ca>
2008-12-02 22:26:50 -0500

    whitespace fixup in userauthorization
    darcs-hash:20081203032650-5ed1f-1f9affd557cf958cdfe7ed224433199b22e1c56a.gz

--- actions/userauthorization.php
+++ actions/userauthorization.php
@@ -415,12 +415,12 @@ class UserauthorizationAction extends Action {
 		if (strlen($listenee) > 255) {
 			throw new OAuthException("Listenee URI '$listenee' too long");
 		}
-		
+
 		$other = User::staticGet('uri', $listenee);
 		if ($other) {
 			throw new OAuthException("Listenee URI '$listenee' is local user");
 		}
-		
+
 		$remote = Remote_profile::staticGet('uri', $listenee);
 		if ($remote) {
 			$sub = new Subscription();
@@ -440,11 +440,11 @@ class UserauthorizationAction extends Action {
 		if (!common_valid_http_url($profile)) {
 			throw new OAuthException("Invalid profile URL '$profile'.");
 		}
-		
+
 		if ($profile == common_local_url('showstream', array('nickname' => $nickname))) {
 			throw new OAuthException("Profile URL '$profile' is for a local user.");
 		}
-		
+
 		$license = $req->get_parameter('omb_listenee_license');
 		if (!common_valid_http_url($license)) {
 			throw new OAuthException("Invalid license URL '$license'.");
