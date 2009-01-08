07e9e2dc3241eef1b803e655a5273f79cb758806
Evan Prodromou <evan@controlyourself.ca>
2009-01-08 20:56:09 +0000

    Remove _darcs from git repository
    Not sure how or why this got in there.

--- _darcs/pristine/db/foreign_services.sql
+++
@@ -1,8 +0,0 @@
-insert into foreign_service
-    (id, name, description, created)
-values
-    ('1','Twitter', 'Twitter Micro-blogging service', now());
-insert into foreign_service
-	(id, name, description, created)
-values
-	('2','Facebook', 'Facebook', now());
