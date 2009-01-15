a0db0e391cdeb626436aa5941770ab1340a8156b
Evan Prodromou <evan@controlyourself.ca>
2009-01-15 23:45:43 +0000

    Move personal group nav menu to its own class

--- lib/personal.php
+++ lib/personal.php
@@ -21,95 +21,10 @@ if (!defined('LACONICA')) { exit(1); }
 
 class PersonalAction extends Action
 {
-
-    function is_readonly()
-    {
-        return true;
-    }
-
     function handle($args)
     {
         parent::handle($args);
         common_set_returnto($this->self_url());
     }
 
-    function views_menu()
-    {
-
-        $user = null;
-        $action = $this->trimmed('action');
-        $nickname = $this->trimmed('nickname');
-
-        if ($nickname) {
-            $user = User::staticGet('nickname', $nickname);
-            $user_profile = $user->getProfile();
-        } else {
-            $user_profile = false;
-        }
-
-        common_element_start('ul', array('id' => 'nav_views'));
-
-        common_menu_item(common_local_url('all', array('nickname' =>
-                                                       $nickname)),
-                         _('Personal'),
-                         sprintf(_('%s and friends'), (($user_profile && $user_profile->fullname) ? $user_profile->fullname : $nickname)),
-                         $action == 'all');
-        common_menu_item(common_local_url('replies', array('nickname' =>
-                                                              $nickname)),
-                         _('Replies'),
-                         sprintf(_('Replies to %s'), (($user_profile && $user_profile->fullname) ? $user_profile->fullname : $nickname)),
-                         $action == 'replies');
-        common_menu_item(common_local_url('showstream', array('nickname' =>
-                                                              $nickname)),
-                         _('Profile'),
-                         ($user_profile && $user_profile->fullname) ? $user_profile->fullname : $nickname,
-                         $action == 'showstream');
-        common_menu_item(common_local_url('showfavorites', array('nickname' =>
-                                                              $nickname)),
-                         _('Favorites'),
-                         sprintf(_('%s\'s favorite notices'), ($user_profile) ? $user_profile->getBestName() : _('User')),
-                         $action == 'showfavorites');
-
-        $cur = common_current_user();
-
-        if ($cur && $cur->id == $user->id) {
-
-            common_menu_item(common_local_url('inbox', array('nickname' =>
-                                                                     $nickname)),
-                             _('Inbox'),
-                             _('Your incoming messages'),
-                             $action == 'inbox');
-            common_menu_item(common_local_url('outbox', array('nickname' =>
-                                                                     $nickname)),
-                             _('Outbox'),
-                             _('Your sent messages'),
-                             $action == 'outbox');
-        }
-
-        common_element_end('ul');
-    }
-
-    function source_link($source)
-    {
-        $source_name = _($source);
-        switch ($source) {
-         case 'web':
-         case 'xmpp':
-         case 'mail':
-         case 'omb':
-         case 'api':
-            common_element('span', 'noticesource', $source_name);
-            break;
-         default:
-            $ns = Notice_source::staticGet($source);
-            if ($ns) {
-                common_element('a', array('href' => $ns->url),
-                               $ns->name);
-            } else {
-                common_element('span', 'noticesource', $source_name);
-            }
-            break;
-        }
-        return;
-    }
 }
