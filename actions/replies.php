d3f27765d95d0c4e0c5d8f224822c469ea64660b
Evan Prodromou <evan@controlyourself.ca>
2009-01-17 23:17:57 +0000

    Adapt replies to new framework

--- actions/replies.php
+++ actions/replies.php
@@ -1,9 +1,12 @@
 <?php
-/*
- * Laconica - a distributed open-source microblogging tool
- * Copyright (C) 2008, Controlez-Vous, Inc.
+/**
+ * Laconica, the distributed open-source microblogging tool
  *
- * This program is free software: you can redistribute it and/or modify
+ * List of replies
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -15,86 +18,177 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Personal
+ * @package   Laconica
+ * @author    Evan Prodromou <evan@controlyourself.ca>
+ * @copyright 2008-2009 Control Yourself, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      http://laconi.ca/
  */
 
-if (!defined('LACONICA')) { exit(1); }
+if (!defined('LACONICA')) {
+    exit(1);
+}
 
-require_once(INSTALLDIR.'/actions/showstream.php');
+require_once INSTALLDIR.'/lib/personalgroupnav.php';
+require_once INSTALLDIR.'/lib/noticelist.php';
+require_once INSTALLDIR.'/lib/feedlist.php';
 
-class RepliesAction extends StreamAction
+/**
+ * List of replies
+ *
+ * @category Personal
+ * @package  Laconica
+ * @author   Evan Prodromou <evan@controlyourself.ca>
+ * @license  http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link     http://laconi.ca/
+ */
+
+class RepliesAction extends Action
 {
+    var $user = null;
+    var $page = null;
 
-    function handle($args)
+    /**
+     * Prepare the object
+     *
+     * Check the input values and initialize the object.
+     * Shows an error page on bad input.
+     *
+     * @param array $args $_REQUEST data
+     *
+     * @return boolean success flag
+     */
+
+    function prepare($args)
     {
-
-        parent::handle($args);
+        parent::prepare($args);
 
         $nickname = common_canonical_nickname($this->arg('nickname'));
-        $user = User::staticGet('nickname', $nickname);
 
-        if (!$user) {
-            $this->no_such_user();
-            return;
+        $this->user = User::staticGet('nickname', $nickname);
+
+        if (!$this->user) {
+            $this->clientError(_('No such user.'));
+            return false;
         }
 
-        $profile = $user->getProfile();
+        $profile = $this->user->getProfile();
 
         if (!$profile) {
             $this->serverError(_('User has no profile.'));
-            return;
+            return false;
         }
 
-        # Looks like we're good; show the header
+        $this->page = ($this->arg('page')) ? ($this->arg('page')+0) : 1;
 
-        common_show_header(sprintf(_("Replies to %s"), $profile->nickname),
-                           array($this, 'show_header'), $user,
-                           array($this, 'show_top'));
-
-        $this->show_replies($user);
-
-        common_show_footer();
+        return true;
     }
 
-    function no_such_user()
+    /**
+     * Handle a request
+     *
+     * Just show the page. All args already handled.
+     *
+     * @param array $args $_REQUEST data
+     *
+     * @return void
+     */
+
+    function handle($args)
     {
-        $this->clientError(_('No such user.'));
+        parent::handle($args);
+        $this->showPage();
     }
 
-    function show_header($user)
+    /**
+     * Title of the page
+     *
+     * Includes name of user and page number.
+     *
+     * @return string title of page
+     */
+
+    function title()
     {
+        if ($this->page == 1) {
+            return sprintf(_("Replies to %s"), $this->user->nickname);
+        } else {
+            return sprintf(_("Replies to %s, page %d"),
+                           $profile->nickname,
+                           $this->page);
+        }
+    }
+
+    /**
+     * Feeds for the <head> section
+     *
+     * @return void
+     */
+
+    function showFeeds()
+    {
+        $rssurl   = common_local_url('repliesrss',
+                                     array('nickname' => $this->user->nickname));
+        $rsstitle = sprintf(_('Feed for replies to %s'), $this->user->nickname);
+
         $this->element('link', array('rel' => 'alternate',
-                                     'href' => common_local_url('repliesrss', array('nickname' =>
-                                                                                    $user->nickname)),
+                                     'href' => $rssurl,
                                      'type' => 'application/rss+xml',
-                                     'title' => sprintf(_('Feed for replies to %s'), $user->nickname)));
+                                     'title' => $rsstitle));
     }
 
-    function show_top($user)
+    /**
+     * show the personal group nav
+     *
+     * @return void
+     */
+
+    function showLocalNav()
     {
-        $cur = common_current_user();
-
-        if ($cur && $cur->id == $user->id) {
-            common_notice_form('replies');
-        }
-
-        $this->views_menu();
-
-        $this->show_feeds_list(array(0=>array('href'=>common_local_url('repliesrss', array('nickname' => $user->nickname)),
-                                              'type' => 'rss',
-                                              'version' => 'RSS 1.0',
-                                              'item' => 'repliesrss')));
+        $nav = new PersonalGroupNav($this);
+        $nav->show();
     }
 
-    function show_replies($user)
+    /**
+     * Show the replies feed links
+     *
+     * @return void
+     */
+
+    function showExportData()
     {
+        $fl = new FeedList($this);
 
-        $page = ($this->arg('page')) ? ($this->arg('page')+0) : 1;
+        $rssurl = common_local_url('repliesrss',
+                                   array('nickname' => $this->user->nickname));
 
-        $notice = $user->getReplies(($page-1) * NOTICES_PER_PAGE, NOTICES_PER_PAGE + 1);
+        $fl->show(array(0=>array('href'=> $rssurl,
+                                 'type' => 'rss',
+                                 'version' => 'RSS 1.0',
+                                 'item' => 'repliesrss')));
+    }
 
-        $cnt = $this->show_notice_list($notice);
+    /**
+     * Show the content
+     *
+     * A list of notices that are replies to the user, plus pagination.
+     *
+     * @return void
+     */
 
-        common_pagination($page > 1, $cnt > NOTICES_PER_PAGE,
-                          $page, 'replies', array('nickname' => $user->nickname));
+    function showContent()
+    {
+        $notice = $this->user->getReplies(($this->page-1) * NOTICES_PER_PAGE,
+                                          NOTICES_PER_PAGE + 1);
+
+        $nl = new NoticeList($notice, $this);
+
+        $cnt = $nl->show();
+
+        $this->pagination($this->page > 1, $cnt > NOTICES_PER_PAGE,
+                          $this->page, 'replies',
+                          array('nickname' => $this->user->nickname));
     }
 }
