a7ea08b73adb313071a94cf8307f56b1018f39ee
Evan Prodromou <evan@controlyourself.ca>
2009-01-21 02:55:55 -0500

    Add group by id for group permalinks

---
+++ actions/groupbyid.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Laconica, the distributed open-source microblogging tool
+ *
+ * Permalink for group
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Group
+ * @package   Laconica
+ * @author    Evan Prodromou <evan@controlyourself.ca>
+ * @author    Sarven Capadisli <csarven@controlyourself.ca>
+ * @copyright 2008-2009 Control Yourself, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      http://laconi.ca/
+ */
+
+if (!defined('LACONICA')) {
+    exit(1);
+}
+
+require_once INSTALLDIR.'/lib/noticelist.php';
+require_once INSTALLDIR.'/lib/feedlist.php';
+
+/**
+ * Permalink for a group
+ *
+ * The group nickname can change, but not the group ID. So we use
+ * an URL with the ID in it as the permanent identifier.
+ *
+ * @category Group
+ * @package  Laconica
+ * @author   Evan Prodromou <evan@controlyourself.ca>
+ * @license  http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link     http://laconi.ca/
+ */
+
+class GroupbyidAction extends Action
+{
+    /** group we're viewing. */
+    var $group = null;
+
+    /**
+     * Is this page read-only?
+     *
+     * @return boolean true
+     */
+
+    function isReadOnly()
+    {
+        return true;
+    }
+
+    function prepare($args)
+    {
+        parent::prepare($args);
+
+        if (!common_config('inboxes','enabled')) {
+            $this->serverError(_('Inboxes must be enabled for groups to work'));
+            return false;
+        }
+
+        $id = $this->arg('id');
+
+        if (!$id) {
+            $this->clientError(_('No ID'));
+            return false;
+        }
+
+        common_debug("Got ID $id");
+
+        $this->group = User_group::staticGet('id', $id);
+
+        if (!$this->group) {
+            $this->clientError(_('No such group'), 404);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle the request
+     *
+     * Shows a profile for the group, some controls, and a list of
+     * group notices.
+     *
+     * @return void
+     */
+
+    function handle($args)
+    {
+        common_redirect($this->group->homeUrl(), 303);
+    }
+}
