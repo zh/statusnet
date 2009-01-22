0df954aa89ed33a64bd519222a0d79c884025ac6
Evan Prodromou <evan@controlyourself.ca>
2009-01-22 17:19:27 +0100

    Make subscribers work

--- actions/subscribers.php
+++ actions/subscribers.php
@@ -1,9 +1,12 @@
 <?php
-/*
- * Laconica - a distributed open-source microblogging tool
- * Copyright (C) 2008, Controlez-Vous, Inc.
+/**
+ * Laconica, the distributed open-source microblogging tool
  *
- * This program is free software: you can redistribute it and/or modify
+ * List a user's subscribers
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -15,56 +18,85 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Social
+ * @package   Laconica
+ * @author    Evan Prodromou <evan@controlyourself.ca>
+ * @author    Sarven Capadisli <csarven@controlyourself.ca>
+ * @copyright 2008-2009 Control Yourself, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      http://laconi.ca/
  */
 
-if (!defined('LACONICA')) { exit(1); }
+if (!defined('LACONICA')) {
+    exit(1);
+}
 
-require_once(INSTALLDIR.'/lib/gallery.php');
+/**
+ * List a user's subscribers
+ *
+ * @category Social
+ * @package  Laconica
+ * @author   Evan Prodromou <evan@controlyourself.ca>
+ * @license  http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link     http://laconi.ca/
+ */
 
 class SubscribersAction extends GalleryAction
 {
-
-    function gallery_type()
+    function title()
     {
-        return _('Subscribers');
-    }
-
-    function get_instructions(&$profile)
-    {
-        $user =& common_current_user();
-        if ($user && ($user->id == $profile->id)) {
-            return _('These are the people who listen to your notices.');
+        if ($this->page == 1) {
+            return sprintf(_('%s subscribers'), $this->user->nickname);
         } else {
-            return sprintf(_('These are the people who listen to %s\'s notices.'), $profile->nickname);
+            return sprintf(_('%s subscribers, page %d'),
+                           $this->user->nickname,
+                           $this->page);
         }
     }
 
-    function fields()
+    function showPageNotice()
     {
-        return array('subscriber', 'subscribed');
+        $user =& common_current_user();
+        if ($user && ($user->id == $this->profile->id)) {
+            $this->element('p', null,
+                           _('These are the people who listen to '.
+                             'your notices.'));
+        } else {
+            $this->element('p', null,
+                           sprintf(_('These are the people who '.
+                                     'listen to %s\'s notices.'),
+                                   $this->profile->nickname));
+        }
     }
 
-    function div_class()
+    function showContent()
     {
-        return 'subscribers';
-    }
+        $offset = ($this->page-1) * PROFILES_PER_PAGE;
+        $limit =  PROFILES_PER_PAGE + 1;
 
-    function get_other(&$subs)
-    {
-        return $subs->subscriber;
-    }
+        $subscribers = $this->user->getSubscribers($offset, $limit);
 
-    function profile_list_class()
-    {
-        return 'SubscribersList';
+        if ($subscribers) {
+            $subscribers_list = new SubscribersList($subscribers, $this->user, $this);
+            $subscribers_list->show();
+        }
+
+        $subscribers->free();
+
+        $this->pagination($this->page > 1, $cnt > PROFILES_PER_PAGE,
+                          $this->page, 'subscribers',
+                          array('nickname' => $this->user->nickname));
     }
 }
 
 class SubscribersList extends ProfileList
 {
-    function show_owner_controls($profile)
+    function showOwnerControls($profile)
     {
-        common_block_form($profile, array('action' => 'subscribers',
-                                          'nickname' => $this->owner->nickname));
+        $bf = new BlockForm($this->out, $profile,
+                            array('action' => 'subscribers',
+                                  'nickname' => $this->owner->nickname));
+        $bf->show();
     }
 }
