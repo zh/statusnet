c1bc77efd968aecd465fb6173d20bd386016eae2
Evan Prodromou <evan@controlyourself.ca>
2009-02-09 12:06:06 -0500

    whitespace and formatting

--- lib/imagefile.php
+++ lib/imagefile.php
@@ -68,17 +68,17 @@ class ImageFile
     static function fromUpload($param='upload')
     {
         switch ($_FILES[$param]['error']) {
-        case UPLOAD_ERR_OK: // success, jump out
+         case UPLOAD_ERR_OK: // success, jump out
             break;
-        case UPLOAD_ERR_INI_SIZE:
-        case UPLOAD_ERR_FORM_SIZE:
+         case UPLOAD_ERR_INI_SIZE:
+         case UPLOAD_ERR_FORM_SIZE:
             throw new Exception(sprintf(_('That file is too big. The maximum file size is %d.'), $this->maxFileSize()));
             return;
-        case UPLOAD_ERR_PARTIAL:
+         case UPLOAD_ERR_PARTIAL:
             @unlink($_FILES[$param]['tmp_name']);
             throw new Exception(_('Partial upload.'));
             return;
-        default:
+         default:
             throw new Exception(_('System error uploading file.'));
             return;
         }
@@ -112,19 +112,19 @@ class ImageFile
             throw new Exception(_('Lost our file.'));
             return;
         }
-        
+
         // Don't crop/scale if it isn't necessary
-        if ($size === $this->width 
-            && $size === $this->height 
-            && $x === 0 
-            && $y === 0 
+        if ($size === $this->width
+            && $size === $this->height
+            && $x === 0
+            && $y === 0
             && $w === $this->width
             && $h === $this->height) {
-            
+
             $outname = Avatar::filename($this->id,
-                                                image_type_to_extension($this->type),
-                                                $size,
-                                                common_timestamp());
+                                        image_type_to_extension($this->type),
+                                        $size,
+                                        common_timestamp());
             $outpath = Avatar::path($outname);
             @copy($this->filepath, $outpath);
             return $outname;
@@ -171,9 +171,9 @@ class ImageFile
         imagecopyresampled($image_dest, $image_src, 0, 0, $x, $y, $size, $size, $w, $h);
 
         $outname = Avatar::filename($this->id,
-                                          image_type_to_extension($this->type),
-                                          $size,
-                                          common_timestamp());
+                                    image_type_to_extension($this->type),
+                                    $size,
+                                    common_timestamp());
 
         $outpath = Avatar::path($outname);
 
@@ -191,7 +191,7 @@ class ImageFile
             throw new Exception(_('Unknown file type'));
             return;
         }
-        
+
         imagedestroy($image_src);
         imagedestroy($image_dest);
 
@@ -229,12 +229,12 @@ class ImageFile
         $num = substr($str, 0, -1);
 
         switch(strtoupper($unit)){
-            case 'G':
-                $num *= 1024;
-            case 'M':
-                $num *= 1024;
-            case 'K':
-                $num *= 1024;
+         case 'G':
+            $num *= 1024;
+         case 'M':
+            $num *= 1024;
+         case 'K':
+            $num *= 1024;
         }
 
         return $num;
