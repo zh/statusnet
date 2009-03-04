00c358956fcece251b8d78f2c6a41098571472c7
Evan Prodromou <evan@controlyourself.ca>
2009-03-04 12:07:53 -0800

    check for profile record

--- lib/noticesection.php
+++ lib/noticesection.php
@@ -73,6 +73,11 @@ class NoticeSection extends Section
     function showNotice($notice)
     {
         $profile = $notice->getProfile();
+        if (empty($profile)) {
+            common_log(LOG_WARNING, sprintf("Notice %d has no profile",
+                                            $notice->id));
+            return;
+        }
         $this->out->elementStart('li', 'hentry notice');
         $this->out->elementStart('div', 'entry-title');
         $avatar = $profile->getAvatar(AVATAR_MINI_SIZE);
