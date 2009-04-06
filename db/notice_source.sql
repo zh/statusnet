f09886672bc03220768fd5818a9e74bb60cc3172
Evan Prodromou <evan@controlyourself.ca>
2009-04-06 15:51:57 -0400

    add peoplebrowsr

--- db/notice_source.sql
+++ db/notice_source.sql
@@ -21,6 +21,7 @@ VALUES
     ('mbpidgin','mbpidgin','http://code.google.com/p/microblog-purple/', now()),
     ('Mobidentica', 'Mobidentica', 'http://www.substanceofcode.com/software/mobidentica/', now()),
     ('moconica','Moconica','http://moconica.com/', now()),
+    ('peoplebrowsr', 'PeopleBrowsr', 'http://www.peoplebrowsr.com/', now()),
     ('pocketwit','PockeTwit','http://code.google.com/p/pocketwit/', now()),
     ('posty','Posty','http://spreadingfunkyness.com/posty/', now()),
     ('royalewithcheese','Royale With Cheese','http://p.hellyeah.org/', now()),
