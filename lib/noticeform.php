0bc9b2e730bb6368d36ba5bb3f2df1bf1432adad
Sarven Capadisli <csarven@controlyourself.ca>
2009-06-15 03:21:15 +0000

    Cross-browser notice_attach

--- lib/noticeform.php
+++ lib/noticeform.php
@@ -148,12 +148,12 @@ class NoticeForm extends Form
         $this->out->element('dd', array('id' => 'notice_text-count'),
                             '140');
         $this->out->elementEnd('dl');
-        $this->out->hidden('MAX_FILE_SIZE', common_config('attachments', 'file_quota'));
-        $this->out->element('label', array('for' => 'notice_data-attach'), _('Attach'));
+        $this->out->element('label', array('for' => 'notice_data-attach'),_('Attach'));
         $this->out->element('input', array('id' => 'notice_data-attach',
                                            'type' => 'file',
                                            'name' => 'attach',
                                            'title' => _('Attach a file')));
+        $this->out->hidden('MAX_FILE_SIZE', common_config('attachments', 'file_quota'));
         if ($this->action) {
             $this->out->hidden('notice_return-to', $this->action, 'returnto');
         }
