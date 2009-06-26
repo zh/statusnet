9fc8ad4531595aa4530876630a5a3c7a88294a8c
Sarven Capadisli <csarven@controlyourself.ca>
2009-06-26 15:10:04 +0000

    Removed redundant selector from JS

--- js/userdesign.go.js
+++ js/userdesign.go.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         C = $(S).val();
         switch (parseInt(S.id.slice(-1))) {
             case 1: default:
-                $('html, body').css({'background-color':C});
+                $('body').css({'background-color':C});
                 break;
             case 2:
                 $('#content, #site_nav_local_views .current a').css({'background-color':C});
