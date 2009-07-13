ae2bc6a8500f2878ed376f60c81c3c53c8ab5b87
Toby Inkster <mail@tobyinkster.co.uk>
2009-07-13 15:37:15 +0100

    Add notice tags to RSS 1.0 output.

--- lib/rssaction.php
+++ lib/rssaction.php
@@ -192,6 +192,24 @@ class Rss10Action extends Action
         }
     }
 
+    // XXX: Surely there should be a common function to do this?
+    function extract_tags ($string)
+    {
+        $count = preg_match_all('/(?:^|\s)#([A-Za-z0-9_\-\.]{1,64})/', strtolower($string), $match);
+        if (!count)
+        {
+            return array();
+        }
+
+        $rv = array();
+        foreach ($match[1] as $tag)
+        {
+            $rv[] = common_canonical_tag($tag);
+        } 
+
+        return array_unique($rv);
+    }
+	 
     function showItem($notice)
     {
         $profile = Profile::staticGet($notice->profile_id);
@@ -245,7 +263,20 @@ class Rss10Action extends Action
                 $this->element('sioc:links_to', array('rdf:resource'=>$attachment->url));
             }
         }
-
+        $tags = $this->extract_tags($notice->content);
+        if (!empty($tags)) {
+            foreach ($tags as $tag)
+            {
+                $tagpage = common_local_url('tag', array('tag' => $tag));
+                $tagrss  = common_local_url('tagrss', array('tag' => $tag));
+                $this->elementStart('ctag:tagged');
+                $this->elementStart('ctag:Tag', array('rdf:about'=>$tagpage.'#concept', 'ctag:label'=>$tag));
+                $this->element('foaf:page', array('rdf:resource'=>$tagpage));
+                $this->element('rdfs:seeAlso', array('rdf:resource'=>$tagrss));
+                $this->elementEnd('ctag:Tag');
+                $this->elementEnd('ctag:tagged');
+            }
+        }
         $this->elementEnd('item');
         $this->creators[$creator_uri] = $profile;
     }
@@ -281,6 +312,8 @@ class Rss10Action extends Action
                                               'http://creativecommons.org/ns#',
                                               'xmlns:content' =>
                                               'http://purl.org/rss/1.0/modules/content/',
+                                              'xmlns:ctag' =>
+                                              'http://commontag.org/ns#',
                                               'xmlns:foaf' =>
                                               'http://xmlns.com/foaf/0.1/',
                                               'xmlns:sioc' =>
