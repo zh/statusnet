75545cdc07db0e12a351ed0f519675626dd2e7bd
Craig Andrews <candrews@integralblue.com>
2009-07-15 10:37:50 -0400

    Added attachment information to the json responses

--- lib/twitterapi.php
+++ lib/twitterapi.php
@@ -186,6 +186,21 @@ class TwitterapiAction extends Action
             $twitter_status['favorited'] = false;
         }
 
+        # Enclosures
+        $attachments = $notice->attachments();
+        $twitter_status['attachments']=array();
+        if($attachments){
+            foreach($attachments as $attachment){
+                if ($attachment->isEnclosure()) {
+                    $enclosure=array();
+                    $enclosure['url']=$attachment->url;
+                    $enclosure['mimetype']=$attachment->mimetype;
+                    $enclosure['size']=$attachment->size;
+                    $twitter_status['attachments'][]=$enclosure;
+                }
+            }
+        }
+
         if ($include_user) {
             # Don't get notice (recursive!)
             $twitter_user = $this->twitter_user_array($profile, false);
