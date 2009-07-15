b5fa0ac1d1b6b3e4ecd0f39fe086702cf19eccdf
Brenda Wallace <shiny@cpan.org>
2009-07-15 18:48:39 +1200

    added missing conversation id to notices
    Conflicts:
    
    	db/laconica_pg.sql

--- db/laconica_pg.sql
+++ db/laconica_pg.sql
@@ -120,7 +120,6 @@ create table notice (
     conversation integer /*id of root notice in this conversation' */ references notice (id)
 
 
-
 /*    FULLTEXT(content) */
 );
 create index notice_profile_id_idx on notice using btree(profile_id);
