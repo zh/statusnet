932d1fb7ce06081c788ea088171fd69ef03cd394
Craig Andrews <candrews@integralblue.com>
2009-08-06 15:26:13 -0400

    Move inline javascript to a separate js file

--- plugins/InfiniteScroll/InfiniteScrollPlugin.php
+++ plugins/InfiniteScroll/InfiniteScrollPlugin.php
@@ -41,22 +41,6 @@ class InfiniteScrollPlugin extends Plugin
     function onEndShowScripts($action)
     {
         $action->script('plugins/InfiniteScroll/jquery.infinitescroll.min.js');
-        $loading_image = common_path('plugins/InfiniteScroll/ajax-loader.gif');
-        $js_string = <<<EOT
-<script type="text/javascript">
-jQuery(document).ready(function($){
-  $('notices_primary').infinitescroll({
-    nextSelector    : "li.nav_next a",
-    loadingImg      : "$loading_image",
-    text            : "<em>Loading the next set of posts...</em>",
-    donetext        : "<em>Congratulations, you\'ve reached the end of the Internet.</em>",
-    navSelector     : "div.pagination",
-    contentSelector : "#notices_primary",
-    itemSelector    : "ol.notices"
-    });
-});
-</script>
-EOT;
-        $action->raw($js_string);
+        $action->script('plugins/InfiniteScroll/infinitescroll.js');
     }
 }
