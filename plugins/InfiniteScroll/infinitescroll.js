2260cf20f5cbbda13fa470b55509c89f4f1e08e7
Craig Andrews <candrews@integralblue.com>
2009-08-11 23:05:46 -0400

    Corrected the Infinite Scroll CSS Selectors
    Thanks dcostalis for making me aware of the issue!

--- plugins/InfiniteScroll/infinitescroll.js
+++ plugins/InfiniteScroll/infinitescroll.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($){
   $('notices_primary').infinitescroll({
+    debug: true,
     nextSelector    : "li.nav_next a",
     loadingImg      : $('address .url')[0].href+'plugins/InfiniteScroll/ajax-loader.gif',
     text            : "<em>Loading the next set of posts...</em>",
     donetext        : "<em>Congratulations, you\'ve reached the end of the Internet.</em>",
     navSelector     : "div.pagination",
-    contentSelector : "#notices_primary",
-    itemSelector    : "ol.notices"
+    contentSelector : "#notices_primary ol.notices",
+    itemSelector    : "#notices_primary ol.notices li"
     },function(){
         NoticeAttachments();
     });
