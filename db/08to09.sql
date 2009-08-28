8689955e0879d160f3fea31c3abdb97d64bc294f
Brenda Wallace <shiny@cpan.org>
2009-08-29 09:48:30 +1200

    added missing parts to postgres update, and the config+user_role tables to both upgrade scripts

--- db/08to09.sql
+++ db/08to09.sql
@@ -12,3 +12,23 @@ alter table user_group
 
 alter table file_oembed
      add column mimetype varchar(50) comment 'mime type of resource';
+
+create table config (
+
+    section varchar(32) comment 'configuration section',
+    setting varchar(32) comment 'configuration setting',
+    value varchar(255) comment 'configuration value',
+
+    constraint primary key (section, setting)
+
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
+
+create table user_role (
+
+    user_id integer not null comment 'user having the role' references user (id),
+    role    varchar(32) not null comment 'string representing the role',
+    created datetime not null comment 'date the role was granted',
+
+    constraint primary key (user_id, role)
+
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
