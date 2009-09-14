4e033138b31ffbc9a0f7d75d8bea7518de617b6c
Zach Copley <zach@controlyourself.ca>
2009-09-14 01:39:54 +0000

    Test action to simulate an aggregator. Useful for checking that the cloud hub is sending notifications.

---
+++ plugins/RSSCloud/LoggingAggregator.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * This test class pretends to be an RSS aggregator. It logs notifications
+ * from the cloud.
+ *
+ * PHP version 5
+ *
+ * @category Plugin
+ * @package  StatusNet
+ * @author   Zach Copley <zach@status.net>
+ * @license  http://www.fsf.org/licensing/licenses/agpl.html AGPLv3
+ * @link     http://status.net/
+ *
+ * StatusNet - the distributed open-source microblogging tool
+ * Copyright (C) 2009, StatusNet, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!defined('STATUSNET')) {
+    exit(1);
+}
+
+class LoggingAggregatorAction extends Action
+{
+
+    /**
+     * Initialization.
+     *
+     * @param array $args Web and URL arguments
+     *
+     * @return boolean false if user doesn't exist
+     */
+    function prepare($args)
+    {
+        parent::prepare($args);
+        return true;
+    }
+
+    function handle($args)
+    {
+        parent::handle($args);
+
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+			$this->showError('This resource requires an HTTP POST.');
+        }
+
+        $this->url = $this->arg('url');
+
+        if (empty($this->url)) {
+            $this->showError('Hey, you have to provide a url parameter.');
+        }
+
+        $this->ip  = $_SERVER['REMOTE_ADDR'];
+
+        common_log(LOG_INFO, 'RSSCloud Logging Aggregator - ' . $this->ip . ' claims the feed at ' .
+                   $this->url . ' has been updated.');
+
+        header('Content-Type: text/xml');
+        echo '<notifyResult success=\'true\' msg=\'Thanks for the update.\' />' . "\n";
+
+	}
+
+    function showError($msg)
+    {
+        header('HTTP/1.1 403 Forbidden');
+        header('Content-Type: text/xml');
+        echo "<?xml version='1.0'?>\n";
+        echo "<notifyResult success='false' msg='$msg' />\n";
+        exit();
+    }
+
+}
