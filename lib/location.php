b6b245dc0be75a3358fee560a40a54e280e4f2a7
Evan Prodromou <evan@status.net>
2009-09-15 20:27:11 -0400

    start of a location class

---
+++ lib/location.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * StatusNet, the distributed open-source microblogging tool
+ *
+ * Class for locations
+ *
+ * PHP version 5
+ *
+ * LICENCE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Location
+ * @package   StatusNet
+ * @author    Evan Prodromou <evan@status.net>
+ * @copyright 2009 StatusNet, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link      http://status.net/
+ */
+
+if (!defined('STATUSNET') && !defined('LACONICA')) {
+    exit(1);
+}
+
+/**
+ * class for locations
+ *
+ * @category Location
+ * @package  StatusNet
+ * @author   Evan Prodromou <evan@status.net>
+ * @license  http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
+ * @link     http://status.net/
+ */
+
+class Location
+{
+    public $lat;
+    public $lon;
+    public $location_id;
+    public $location_ns;
+
+    var $names;
+
+    static function fromName($name, $language=null)
+    {
+        if (is_null($language)) {
+            $language = common_language();
+        }
+    }
+
+    static function fromId($location_id, $location_ns = null)
+    {
+        if (is_null($location_ns)) {
+            $location_ns = common_config('location', 'namespace');
+        }
+    }
+
+    function getName($language=null)
+    {
+        if (is_null($language)) {
+            $language = common_language();
+        }
+
+        if (array_key_exists($this->names, $language)) {
+            return $this->names[$language];
+        }
+    }
+}
