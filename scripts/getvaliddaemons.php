fe4751de50f2c26953dca79ab51543f8adb12353
Evan Prodromou <evan@status.net>
2009-09-21 14:44:16 -0400

    add the plugin daemon

--- scripts/getvaliddaemons.php
+++ scripts/getvaliddaemons.php
@@ -37,22 +37,27 @@ require_once INSTALLDIR.'/scripts/commandline.inc';
 
 $daemons = array();
 
+$daemons[] = INSTALLDIR.'/scripts/pluginqueuehandler.php';
+$daemons[] = INSTALLDIR.'/scripts/ombqueuehandler.php';
+$daemons[] = INSTALLDIR.'/scripts/facebookqueuehandler.php';
+$daemons[] = INSTALLDIR.'/scripts/pingqueuehandler.php';
+
 if(common_config('xmpp','enabled')) {
     $daemons[] = INSTALLDIR.'/scripts/xmppdaemon.php';
     $daemons[] = INSTALLDIR.'/scripts/jabberqueuehandler.php';
     $daemons[] = INSTALLDIR.'/scripts/publicqueuehandler.php';
     $daemons[] = INSTALLDIR.'/scripts/xmppconfirmhandler.php';
 }
+
 if(common_config('twitterbridge','enabled')) {
     $daemons[] = INSTALLDIR.'/scripts/twitterstatusfetcher.php';
 }
-$daemons[] = INSTALLDIR.'/scripts/ombqueuehandler.php';
+
 if (common_config('twitter', 'enabled')) {
     $daemons[] = INSTALLDIR.'/scripts/twitterqueuehandler.php';
     $daemons[] = INSTALLDIR.'/scripts/synctwitterfriends.php';
 }
-$daemons[] = INSTALLDIR.'/scripts/facebookqueuehandler.php';
-$daemons[] = INSTALLDIR.'/scripts/pingqueuehandler.php';
+
 if (common_config('sms', 'enabled')) {
     $daemons[] = INSTALLDIR.'/scripts/smsqueuehandler.php';
 }
