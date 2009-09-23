35eb33fe2bd0d41fb4c03000c557c36125b2c809
Evan Prodromou <evan@status.net>
2009-09-23 11:08:12 -0400

    Revert "Fixed indenting"
    This reverts commit 182257cbbf1557c2671b1995c9126c45b0bd8e79.

--- plugins/Meteor/meteorupdater.js
+++ plugins/Meteor/meteorupdater.js
@@ -4,28 +4,28 @@
 
 var MeteorUpdater = function()
 {
-    return {
+     return {
 
-        init: function(server, port, timeline)
-        {
-            var screen_name;
+          init: function(server, port, timeline)
+          {
+               var screen_name;
 
-            Meteor.callbacks["process"] = function(data) {
-                var d = JSON.parse(data);
-                screen_name = d['user']['screen_name'];
+               Meteor.callbacks["process"] = function(data) {
+                    var d = JSON.parse(data);
+                    screen_name = d['user']['screen_name'];
 
-                if (timeline == 'public' ||
-                    $('address .url')[0].href+screen_name+'/all' == window.location.href ||
-                    $('address .url')[0].href+screen_name == window.location.href) {
-                    RealtimeUpdate.receive(d);
-                }
-            };
+                    if (timeline == 'public' ||
+                        $('address .url')[0].href+screen_name+'/all' == window.location.href ||
+                        $('address .url')[0].href+screen_name == window.location.href) {
+                        RealtimeUpdate.receive(d);
+                    }
+               };
 
-            Meteor.host = server;
-            Meteor.port = port;
-            Meteor.joinChannel(timeline, 0);
-            Meteor.connect();
-        }
-    }
+               Meteor.host = server;
+               Meteor.port = port;
+               Meteor.joinChannel(timeline, 0);
+               Meteor.connect();
+          }
+     }
 }();
 
