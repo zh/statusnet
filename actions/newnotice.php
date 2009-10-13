659a04b556f75551548c0b2fff633b15cd114135
Brion Vibber <brion@pobox.com>
2009-10-13 16:54:57 +0000

    Include long-form attachment URL in notice if URL shortening is disabled. Previously, the attachment URL would simply be dropped when shortening returned false instead of a short URL... the attachment was present if you clicked through to notice details but didn't appear in the timeline, making it nigh-impossible to see the attachment.

--- actions/newnotice.php
+++ actions/newnotice.php
@@ -240,6 +240,10 @@ class NewnoticeAction extends Action
             $this->maybeAddRedir($fileRecord->id, $fileurl);
 
             $short_fileurl = common_shorten_url($fileurl);
+            if (!$short_fileurl) {
+                // todo -- Consider forcing default shortener if none selected?
+                $short_fileurl = $fileurl;
+            }
             $content_shortened .= ' ' . $short_fileurl;
 
             if (Notice::contentTooLong($content_shortened)) {
