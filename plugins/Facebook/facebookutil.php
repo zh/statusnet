3e5c8f28c362e945ca7d89e7aaff1e1304a9192a
Zach Copley <zach@status.net>
2009-10-21 17:29:54 +0000

    Fix references

--- plugins/Facebook/facebookutil.php
+++ plugins/Facebook/facebookutil.php
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once INSTALLDIR . '/extlib/facebook/facebook.php';
+require_once INSTALLDIR . '/plugins/Facebook/facebook/facebook.php';
 require_once INSTALLDIR . '/plugins/Facebook/facebookaction.php';
 require_once INSTALLDIR . '/lib/noticelist.php';
 
