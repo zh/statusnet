b7ed31c27bc883b7b7b6a9af6391aacd8d897d78
Brion Vibber <brion@pobox.com>
2009-10-30 16:53:23 -0400

    Use nickname from profile, not user object, to avoid barfing warnings on Twitter messages imported into the timeline.

--- actions/shownotice.php
+++ actions/shownotice.php
@@ -172,9 +172,9 @@ class ShownoticeAction extends OwnerDesignAction
     function title()
     {
         if (!empty($this->profile->fullname)) {
-            $base = $this->profile->fullname . ' (' . $this->user->nickname . ') ';
+            $base = $this->profile->fullname . ' (' . $this->profile->nickname . ') ';
         } else {
-            $base = $this->user->nickname;
+            $base = $this->profile->nickname;
         }
 
         return sprintf(_('%1$s\'s status on %2$s'),
