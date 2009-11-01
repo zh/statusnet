644263f9894d229bc6b9f03d5590dfecc65f9d6d
Evan Prodromou <evan@status.net>
2009-11-01 14:16:50 -0500

    update autogenerated data objects to match our classes

--- plugins/UserFlag/Notice_flag.php
+++ plugins/UserFlag/Notice_flag.php
@@ -1,21 +1,40 @@
 <?php
-/**
- * Table Definition for notice_flag
+/*
+ * StatusNet - the distributed open-source microblogging tool
+ * Copyright (C) 2009, StatusNet, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once 'classes/Memcached_DataObject.php';
 
-class Notice_flag extends Memcached_DataObject 
+if (!defined('STATUSNET')) {
+    exit(1);
+}
+
+require_once INSTALLDIR . '/classes/Memcached_DataObject.php';
+
+class Notice_flag extends Memcached_DataObject
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
 
     public $__table = 'notice_flag';                     // table name
     public $flag;                            // varchar(8)  primary_key not_null
-    public $display;                         // varchar(255)  
+    public $display;                         // varchar(255)
     public $created;                         // datetime   not_null default_0000-00-00%2000%3A00%3A00
 
     /* Static get */
-    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('Notice_flag',$k,$v); }
+    function staticGet($k,$v=NULL) { return Memcached_DataObject::staticGet('Notice_flag',$k,$v); }
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
--- plugins/UserFlag/User_flag_profile.php
+++ plugins/UserFlag/User_flag_profile.php
@@ -1,10 +1,29 @@
 <?php
-/**
- * Table Definition for user_flag_profile
+/*
+ * StatusNet - the distributed open-source microblogging tool
+ * Copyright (C) 2009, StatusNet, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once 'classes/Memcached_DataObject.php';
 
-class User_flag_profile extends Memcached_DataObject 
+if (!defined('STATUSNET')) {
+    exit(1);
+}
+
+require_once INSTALLDIR . '/classes/Memcached_DataObject.php';
+
+class User_flag_profile extends Memcached_DataObject
 {
     ###START_AUTOCODE
     /* the code below is auto generated do not remove the above tag */
@@ -12,11 +31,11 @@ class User_flag_profile extends Memcached_DataObject
     public $__table = 'user_flag_profile';               // table name
     public $profile_id;                      // int(4)  primary_key not_null
     public $user_id;                         // int(4)  primary_key not_null
-    public $flag;                            // varchar(8)  
+    public $flag;                            // varchar(8)
     public $created;                         // datetime   not_null default_0000-00-00%2000%3A00%3A00
 
     /* Static get */
-    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('User_flag_profile',$k,$v); }
+    function staticGet($k,$v=NULL) { return Memcached_DataObject::staticGet('User_flag_profile',$k,$v); }
 
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
