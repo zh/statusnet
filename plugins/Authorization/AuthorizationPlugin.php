a882d093bc99d1162dac29c161253dc037c314b9
Craig Andrews <candrews@integralblue.com>
2009-11-18 16:12:55 -0500

    Blasted missing a $this!

--- plugins/Authorization/AuthorizationPlugin.php
+++ plugins/Authorization/AuthorizationPlugin.php
@@ -88,7 +88,7 @@ abstract class AuthorizationPlugin extends Plugin
     }
 
     function onStartSetApiUser(&$user) {
-        return onStartSetUser(&$user);
+        return $this->onStartSetUser(&$user);
     }
 
     function onStartHasRole($profile, $name, &$has_role) {
