1827256d0e2b49a77df46f90249c2ab893e0ac4f
Brion Vibber <brion@pobox.com>
2009-11-18 14:57:18 -0800

    Added support for pgettext() and npgettext() to separate contexts for translatable messages that are going to be ambiguous in English original.

--- lib/common.php
+++ lib/common.php
@@ -59,10 +59,6 @@ require_once('PEAR.php');
 require_once('DB/DataObject.php');
 require_once('DB/DataObject/Cast.php'); # for dates
 
-if (!function_exists('gettext')) {
-    require_once("php-gettext/gettext.inc");
-}
-
 require_once(INSTALLDIR.'/lib/language.php');
 
 // This gets included before the config file, so that admin code and plugins
