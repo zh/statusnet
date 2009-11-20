dc90e90fd2748f953a2d4750105817b41cafe43a
Craig Andrews <candrews@integralblue.com>
2009-11-19 19:12:58 -0500

    Load notice data using javascript from the html on the page instead of writing json representations of the notices on each page Clicking on a geo link pops up a map

--- plugins/Mapstraction/usermap.js
+++ plugins/Mapstraction/usermap.js
@@ -1,14 +1,82 @@
 $(document).ready(function() {
-     var mapstraction = new mxn.Mapstraction("map_canvas", _provider);
+     notices = [];
+     $(".notice").each(function(){
+        notice = getNoticeFromElement($(this));
+        if(notice['geo'])
+            notices.push(notice);
+     });
+     if($("#map_canvas") && notices.length>0)
+     {
+        showMapstraction($("#map_canvas"), notices);
+     }
+
+     $('a.geo').click(function(){
+        noticeElement = $(this).closest(".notice");
+        notice = getNoticeFromElement(noticeElement);
+
+        $.fn.jOverlay.options = {
+            color : '#000',
+            opacity : '0.6',
+            zIndex : 99,
+            center : false,
+            bgClickToClose : true,
+            autoHide : true,
+            css : {'max-width':'542px', 'top':'5%', 'left':'32.5%'}
+        };
+        html="<div id='map_canvas_popup' class='gray smallmap' style='width: 542px; height: 500px' />";
+        html+="<button class='close'>&#215;</button>";
+        html+=$("<div/>").append($(this).clone()).html();
+        $().jOverlay({ "html": html });
+        $('#jOverlayContent').show();
+        $('#jOverlayContent button').click($.closeOverlay);
+        
+        showMapstraction($("#map_canvas_popup"), notice);
+
+        return false;
+     });
+});
+
+function getMicroformatValue(element)
+{
+    if(element[0].tagName.toLowerCase() == 'abbr'){
+        return element.attr('title');
+    }else{
+        return element.text();
+    }
+}
+
+function getNoticeFromElement(noticeElement)
+{
+    notice = {};
+    if(noticeElement.find(".latitude").length){
+        notice['geo']={'coordinates': [
+            parseFloat(getMicroformatValue(noticeElement.find(".latitude"))),
+            parseFloat(getMicroformatValue(noticeElement.find(".longitude")))] };
+    }
+    notice['user']={
+        'profile_image_url': noticeElement.find("img.avatar").attr('src'),
+        'profile_url': noticeElement.find(".author a.url").attr('href'),
+        'screen_name': noticeElement.find(".author .nickname").text()
+    };
+    notice['html']=noticeElement.find(".entry-content").html();
+    notice['url']=noticeElement.find("a.timestamp").attr('href');
+    notice['created_at']=noticeElement.find("abbr.published").text();
+    return notice;
+}
+
+function showMapstraction(element, notices) {
+     if(element instanceof jQuery) element = element[0];
+     if(! $.isArray(notices)) notices = [notices];
+     var mapstraction = new mxn.Mapstraction(element, _provider);
 
      var minLat = 181.0;
      var maxLat = -181.0;
      var minLon = 181.0;
      var maxLon = -181.0;
 
-     for (var i in _notices)
+     for (var i in notices)
      {
-          var n = _notices[i];
+          var n = notices[i];
 
           var lat = n['geo']['coordinates'][0];
           var lon = n['geo']['coordinates'][1];
@@ -42,4 +110,4 @@ $(document).ready(function() {
      bounds = new mxn.BoundingBox(minLat, minLon, maxLat, maxLon);
 
      mapstraction.setBounds(bounds);
-});
+}
