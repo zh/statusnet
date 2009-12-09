zh/statusnet

commit 4b5e977a7b1c390555d880d3dc7f8b8c6744646c

New _m() gettext wrapper with smart detection of plugin domains. Plugin base class registers your gettext files if present at initialization.
update_pot.sh replaced with update_po_templates.php which can do core, plugins, or all (default).
Top-level Makefile added to build .mo files for plugins as well as core.

As described on list:
http://lists.status.net/pipermail/statusnet-dev/2009-December/002869.html

lib/language.php
@@ -36,6 +36,33 @@ if (!function_exists('gettext')) {
     require_once("php-gettext/gettext.inc");
 }
 
+
+if (!function_exists('dpgettext')) {
+    /**
+     * Context-aware dgettext wrapper; use when messages in different contexts
+     * won't be distinguished from the English source but need different translations.
+     * The context string will appear as msgctxt in the .po files.
+     *
+     * Not currently exposed in PHP's gettext module; implemented to be compat
+     * with gettext.h's macros.
+     *
+     * @param string $domain domain identifier, or null for default domain
+     * @param string $context context identifier, should be some key like "menu|file"
+     * @param string $msgid English source text
+     * @return string original or translated message
+     */
+    function dpgettext($domain, $context, $msg)
+    {
+        $msgid = $context . "\004" . $msg;
+        $out = dcgettext($domain, $msgid, LC_MESSAGES);
+        if ($out == $msgid) {
+            return $msg;
+        } else {
+            return $out;
+        }
+    }
+}
+
 if (!function_exists('pgettext')) {
     /**
      * Context-aware gettext wrapper; use when messages in different contexts
@@ -51,8 +78,30 @@ if (!function_exists('pgettext')) {
      */
     function pgettext($context, $msg)
     {
+        return dpgettext(textdomain(NULL), $context, $msg);
+    }
+}
+
+if (!function_exists('dnpgettext')) {
+    /**
+     * Context-aware dngettext wrapper; use when messages in different contexts
+     * won't be distinguished from the English source but need different translations.
+     * The context string will appear as msgctxt in the .po files.
+     *
+     * Not currently exposed in PHP's gettext module; implemented to be compat
+     * with gettext.h's macros.
+     *
+     * @param string $domain domain identifier, or null for default domain
+     * @param string $context context identifier, should be some key like "menu|file"
+     * @param string $msg singular English source text
+     * @param string $plural plural English source text
+     * @param int $n number of items to control plural selection
+     * @return string original or translated message
+     */
+    function dnpgettext($domain, $context, $msg, $plural, $n)
+    {
         $msgid = $context . "\004" . $msg;
-        $out = dcgettext(textdomain(NULL), $msgid, LC_MESSAGES);
+        $out = dcngettext($domain, $msgid, $plural, $n, LC_MESSAGES);
         if ($out == $msgid) {
             return $msg;
         } else {
@@ -78,14 +127,78 @@ if (!function_exists('npgettext')) {
      */
     function npgettext($context, $msg, $plural, $n)
     {
-        $msgid = $context . "\004" . $msg;
-        $out = dcngettext(textdomain(NULL), $msgid, $plural, $n, LC_MESSAGES);
-        if ($out == $msgid) {
-            return $msg;
+        return dnpgettext(textdomain(NULL), $msgid, $plural, $n, LC_MESSAGES);
+    }
+}
+
+/**
+ * Shortcut for *gettext functions with smart domain detection.
+ *
+ * If calling from a plugin, this function checks which plugin was
+ * being called from and uses that as text domain, which will have
+ * been set up during plugin initialization.
+ *
+ * Also handles plurals and contexts depending on what parameters
+ * are passed to it:
+ *
+ *   gettext -> _m($msg)
+ *  ngettext -> _m($msg1, $msg2, $n)
+ *  pgettext -> _m($ctx, $msg)
+ * npgettext -> _m($ctx, $msg1, $msg2, $n)
+ *
+ * @fixme may not work properly in eval'd code
+ *
+ * @param string $msg
+ * @return string
+ */
+function _m($msg/*, ...*/)
+{
+    $domain = _mdomain(debug_backtrace(false));
+    $args = func_get_args();
+    switch(count($args)) {
+    case 1: return dgettext($domain, $msg);
+    case 2: return dpgettext($domain, $args[0], $args[1]);
+    case 3: return dngettext($domain, $args[0], $args[1], $args[2]);
+    case 4: return dnpgettext($domain, $args[0], $args[1], $args[2], $args[3]);
+    default: throw new Exception("Bad parameter count to _m()");
+    }
+}
+
+/**
+ * Looks for which plugin we've been called from to set the gettext domain.
+ *
+ * @param array $backtrace debug_backtrace() output
+ * @return string
+ * @private
+ * @fixme could explode if SN is under a 'plugins' folder or share name.
+ */
+function _mdomain($backtrace)
+{
+    /*
+      0 => 
+        array
+          'file' => string '/var/www/mublog/plugins/FeedSub/FeedSubPlugin.php' (length=49)
+          'line' => int 77
+          'function' => string '_m' (length=2)
+          'args' => 
+            array
+              0 => &string 'Feeds' (length=5)
+    */
+    static $cached;
+    $path = $backtrace[0]['file'];
+    if (!isset($cached[$path])) {
+        if (DIRECTORY_SEPARATOR !== '/') {
+            $path = strtr($path, DIRECTORY_SEPARATOR, '/');
+        }
+        $cut = strpos($path, '/plugins/') + 9;
+        $cut2 = strpos($path, '/', $cut);
+        if ($cut && $cut2) {
+            $cached[$path] = substr($path, $cut, $cut2 - $cut);
         } else {
-            return $out;
+            return null;
         }
     }
+    return $cached[$path];
 }
 
 

Makefile
@@ -1,0 +1,18 @@
+# Warning: do not transform tabs to spaces in this file.
+
+all : translations
+
+core_mo = $(patsubst %.po,%.mo,$(wildcard locale/*/LC_MESSAGES/statusnet.po))
+plugin_mo = $(patsubst %.po,%.mo,$(wildcard plugins/*/locale/*/LC_MESSAGES/*.po))
+
+translations : $(core_mo) $(plugin_mo)
+
+clean :
+	rm -f $(core_mo) $(plugin_mo)
+
+updatepo :
+	php scripts/update_po_templates.php --all
+
+%.mo : %.po
+	msgfmt -o $@ $<
+

lib/plugin.php
@@ -65,6 +65,8 @@ class Plugin
                 Event::addHandler(mb_substr($method, 2), array($this, $method));
             }
         }
+
+        $this->setupGettext();
     }
 
     function initialize()
@@ -77,6 +79,22 @@ class Plugin
         return true;
     }
 
+    /**
+     * Checks if this plugin has localization that needs to be set up.
+     * Gettext localizations can be called via the _m() helper function.
+     */
+    protected function setupGettext()
+    {
+        $class = get_class($this);
+        if (substr($class, -6) == 'Plugin') {
+            $name = substr($class, 0, -6);
+            $path = INSTALLDIR . "/plugins/$name/locale";
+            if (file_exists($path) && is_dir($path)) {
+                bindtextdomain($name, $path);
+            }
+        }
+    }
+
     protected function log($level, $msg)
     {
         common_log($level, get_class($this) . ': '.$msg);
@@ -87,3 +105,4 @@ class Plugin
         $this->log(LOG_DEBUG, $msg);
     }
 }
+

plugins/Facebook/FBConnectAuth.php
@@ -48,8 +48,8 @@ class FBConnectauthAction extends Action
             common_log(LOG_WARNING, 'Facebook Connect Plugin - ' .
                        "Failed auth attempt, proxy = $proxy, ip = $ip.");
 
-            $this->clientError(_('You must be logged into Facebook to ' .
-                                 'use Facebook Connect.'));
+            $this->clientError(_m('You must be logged into Facebook to ' .
+                                  'use Facebook Connect.'));
         }
 
         return true;
@@ -74,7 +74,7 @@ class FBConnectauthAction extends Action
                 // We don't want these cookies
                 getFacebook()->clear_cookie_state();
 
-                $this->clientError(_('There is already a local user linked with this Facebook.'));
+                $this->clientError(_m('There is already a local user linked with this Facebook.'));
 
             } else {
 
@@ -87,12 +87,12 @@ class FBConnectauthAction extends Action
 
             $token = $this->trimmed('token');
             if (!$token || $token != common_session_token()) {
-                $this->showForm(_('There was a problem with your session token. Try again, please.'));
+                $this->showForm(_m('There was a problem with your session token. Try again, please.'));
                 return;
             }
             if ($this->arg('create')) {
                 if (!$this->boolean('license')) {
-                    $this->showForm(_('You can\'t register if you don\'t agree to the license.'),
+                    $this->showForm(_m('You can\'t register if you don\'t agree to the license.'),
                                     $this->trimmed('newname'));
                     return;
                 }
@@ -102,7 +102,7 @@ class FBConnectauthAction extends Action
             } else {
                 common_debug('Facebook Connect Plugin - ' .
                              print_r($this->args, true));
-                $this->showForm(_('Something weird happened.'),
+                $this->showForm(_m('Something weird happened.'),
                                 $this->trimmed('newname'));
             }
         } else {
@@ -116,13 +116,13 @@ class FBConnectauthAction extends Action
             $this->element('div', array('class' => 'error'), $this->error);
         } else {
             $this->element('div', 'instructions',
-                           sprintf(_('This is the first time you\'ve logged into %s so we must connect your Facebook to a local account. You can either create a new account, or connect with your existing account, if you have one.'), common_config('site', 'name')));
+                           sprintf(_m('This is the first time you\'ve logged into %s so we must connect your Facebook to a local account. You can either create a new account, or connect with your existing account, if you have one.'), common_config('site', 'name')));
         }
     }
 
     function title()
     {
-        return _('Facebook Account Setup');
+        return _m('Facebook Account Setup');
     }
 
     function showForm($error=null, $username=null)
@@ -150,7 +150,7 @@ class FBConnectauthAction extends Action
                                           'class' => 'form_settings',
                                           'action' => common_local_url('FBConnectAuth')));
         $this->elementStart('fieldset', array('id' => 'settings_facebook_connect_options'));
-        $this->element('legend', null, _('Connection options'));
+        $this->element('legend', null, _m('Connection options'));
         $this->elementStart('ul', 'form_data');
         $this->elementStart('li');
         $this->element('input', array('type' => 'checkbox',
@@ -159,10 +159,10 @@ class FBConnectauthAction extends Action
                                       'name' => 'license',
                                       'value' => 'true'));
         $this->elementStart('label', array('class' => 'checkbox', 'for' => 'license'));
-        $this->text(_('My text and files are available under '));
+        $this->text(_m('My text and files are available under '));
         $this->element('a', array('href' => common_config('license', 'url')),
                        common_config('license', 'title'));
-        $this->text(_(' except this private data: password, email address, IM address, phone number.'));
+        $this->text(_m(' except this private data: password, email address, IM address, phone number.'));
         $this->elementEnd('label');
         $this->elementEnd('li');
         $this->elementEnd('ul');
@@ -170,33 +170,33 @@ class FBConnectauthAction extends Action
         $this->elementStart('fieldset');
         $this->hidden('token', common_session_token());
         $this->element('legend', null,
-                       _('Create new account'));
+                       _m('Create new account'));
         $this->element('p', null,
-                       _('Create a new user with this nickname.'));
+                       _m('Create a new user with this nickname.'));
         $this->elementStart('ul', 'form_data');
         $this->elementStart('li');
-        $this->input('newname', _('New nickname'),
+        $this->input('newname', _m('New nickname'),
                      ($this->username) ? $this->username : '',
-                     _('1-64 lowercase letters or numbers, no punctuation or spaces'));
+                     _m('1-64 lowercase letters or numbers, no punctuation or spaces'));
         $this->elementEnd('li');
         $this->elementEnd('ul');
-        $this->submit('create', _('Create'));
+        $this->submit('create', _m('Create'));
         $this->elementEnd('fieldset');
 
         $this->elementStart('fieldset');
         $this->element('legend', null,
-                       _('Connect existing account'));
+                       _m('Connect existing account'));
         $this->element('p', null,
-                       _('If you already have an account, login with your username and password to connect it to your Facebook.'));
+                       _m('If you already have an account, login with your username and password to connect it to your Facebook.'));
         $this->elementStart('ul', 'form_data');
         $this->elementStart('li');
-        $this->input('nickname', _('Existing nickname'));
+        $this->input('nickname', _m('Existing nickname'));
         $this->elementEnd('li');
         $this->elementStart('li');
-        $this->password('password', _('Password'));
+        $this->password('password', _m('Password'));
         $this->elementEnd('li');
         $this->elementEnd('ul');
-        $this->submit('connect', _('Connect'));
+        $this->submit('connect', _m('Connect'));
         $this->elementEnd('fieldset');
 
         $this->elementEnd('fieldset');
@@ -212,7 +212,7 @@ class FBConnectauthAction extends Action
     function createNewUser()
     {
         if (common_config('site', 'closed')) {
-            $this->clientError(_('Registration not allowed.'));
+            $this->clientError(_m('Registration not allowed.'));
             return;
         }
 
@@ -221,14 +221,14 @@ class FBConnectauthAction extends Action
         if (common_config('site', 'inviteonly')) {
             $code = $_SESSION['invitecode'];
             if (empty($code)) {
-                $this->clientError(_('Registration not allowed.'));
+                $this->clientError(_m('Registration not allowed.'));
                 return;
             }
 
             $invite = Invitation::staticGet($code);
 
             if (empty($invite)) {
-                $this->clientError(_('Not a valid invitation code.'));
+                $this->clientError(_m('Not a valid invitation code.'));
                 return;
             }
         }
@@ -238,17 +238,17 @@ class FBConnectauthAction extends Action
         if (!Validate::string($nickname, array('min_length' => 1,
                                                'max_length' => 64,
                                                'format' => NICKNAME_FMT))) {
-            $this->showForm(_('Nickname must have only lowercase letters and numbers and no spaces.'));
+            $this->showForm(_m('Nickname must have only lowercase letters and numbers and no spaces.'));
             return;
         }
 
         if (!User::allowed_nickname($nickname)) {
-            $this->showForm(_('Nickname not allowed.'));
+            $this->showForm(_m('Nickname not allowed.'));
             return;
         }
 
         if (User::staticGet('nickname', $nickname)) {
-            $this->showForm(_('Nickname already in use. Try another one.'));
+            $this->showForm(_m('Nickname already in use. Try another one.'));
             return;
         }
 
@@ -266,7 +266,7 @@ class FBConnectauthAction extends Action
         $result = $this->flinkUser($user->id, $this->fbuid);
 
         if (!$result) {
-            $this->serverError(_('Error connecting user to Facebook.'));
+            $this->serverError(_m('Error connecting user to Facebook.'));
             return;
         }
 
@@ -286,7 +286,7 @@ class FBConnectauthAction extends Action
         $password = $this->trimmed('password');
 
         if (!common_check_user($nickname, $password)) {
-            $this->showForm(_('Invalid username or password.'));
+            $this->showForm(_m('Invalid username or password.'));
             return;
         }
 
@@ -300,7 +300,7 @@ class FBConnectauthAction extends Action
         $result = $this->flinkUser($user->id, $this->fbuid);
 
         if (!$result) {
-            $this->serverError(_('Error connecting user to Facebook.'));
+            $this->serverError(_m('Error connecting user to Facebook.'));
             return;
         }
 
@@ -320,7 +320,7 @@ class FBConnectauthAction extends Action
         $result = $this->flinkUser($user->id, $this->fbuid);
 
         if (empty($result)) {
-            $this->serverError(_('Error connecting user to Facebook.'));
+            $this->serverError(_m('Error connecting user to Facebook.'));
             return;
         }
 

plugins/Facebook/FBConnectLogin.php
@@ -30,7 +30,7 @@ class FBConnectLoginAction extends Action
         parent::handle($args);
 
         if (common_is_real_login()) {
-            $this->clientError(_('Already logged in.'));
+            $this->clientError(_m('Already logged in.'));
         }
 
         $this->showPage();
@@ -38,7 +38,7 @@ class FBConnectLoginAction extends Action
 
     function getInstructions()
     {
-        return _('Login with your Facebook Account');
+        return _m('Login with your Facebook Account');
     }
 
     function showPageNotice()
@@ -52,7 +52,7 @@ class FBConnectLoginAction extends Action
 
     function title()
     {
-        return _('Facebook Login');
+        return _m('Facebook Login');
     }
 
     function showContent() {

plugins/Facebook/FBConnectSettings.php
@@ -53,7 +53,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
 
     function title()
     {
-        return _('Facebook Connect Settings');
+        return _m('Facebook Connect Settings');
     }
 
     /**
@@ -64,7 +64,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
 
     function getInstructions()
     {
-        return _('Manage how your account connects to Facebook');
+        return _m('Manage how your account connects to Facebook');
     }
 
     /**
@@ -89,7 +89,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
         if (!$flink) {
 
             $this->element('p', 'instructions',
-                _('There is no Facebook user connected to this account.'));
+                _m('There is no Facebook user connected to this account.'));
 
             $this->element('fb:login-button', array('onlogin' => 'goto_login()',
                 'length' => 'long'));
@@ -97,7 +97,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
         } else {
 
             $this->element('p', 'form_note',
-                           _('Connected Facebook user'));
+                           _m('Connected Facebook user'));
 
             $this->elementStart('p', array('class' => 'facebook-user-display'));
             $this->elementStart('fb:profile-pic',
@@ -116,18 +116,18 @@ class FBConnectSettingsAction extends ConnectSettingsAction
 
             $this->elementStart('fieldset');
 
-            $this->element('legend', null, _('Disconnect my account from Facebook'));
+            $this->element('legend', null, _m('Disconnect my account from Facebook'));
 
             if (!$user->password) {
 
                 $this->elementStart('p', array('class' => 'form_guide'));
-                $this->text(_('Disconnecting your Faceboook ' .
-                              'would make it impossible to log in! Please '));
+                $this->text(_m('Disconnecting your Faceboook ' .
+                               'would make it impossible to log in! Please '));
                 $this->element('a',
                     array('href' => common_local_url('passwordsettings')),
-                        _('set a password'));
+                        _m('set a password'));
 
-                $this->text(_(' first.'));
+                $this->text(_m(' first.'));
                 $this->elementEnd('p');
             } else {
 
@@ -139,7 +139,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
                 $this->element('p', 'instructions',
                     sprintf($note, $site, $site));
 
-                $this->submit('disconnect', _('Disconnect'));
+                $this->submit('disconnect', _m('Disconnect'));
             }
 
             $this->elementEnd('fieldset');
@@ -161,8 +161,8 @@ class FBConnectSettingsAction extends ConnectSettingsAction
         // CSRF protection
         $token = $this->trimmed('token');
         if (!$token || $token != common_session_token()) {
-            $this->showForm(_('There was a problem with your session token. '.
-                              'Try again, please.'));
+            $this->showForm(_m('There was a problem with your session token. '.
+                               'Try again, please.'));
             return;
         }
 
@@ -175,7 +175,7 @@ class FBConnectSettingsAction extends ConnectSettingsAction
 
             if ($result === false) {
                 common_log_db_error($user, 'DELETE', __FILE__);
-                $this->serverError(_('Couldn\'t delete link to Facebook.'));
+                $this->serverError(_m('Couldn\'t delete link to Facebook.'));
                 return;
             }
 
@@ -191,10 +191,10 @@ class FBConnectSettingsAction extends ConnectSettingsAction
                            $e->getMessage());
             }
 
-            $this->showForm(_('You have disconnected from Facebook.'), true);
+            $this->showForm(_m('You have disconnected from Facebook.'), true);
 
         } else {
-            $this->showForm(_('Not sure what you\'re trying to do.'));
+            $this->showForm(_m('Not sure what you\'re trying to do.'));
             return;
         }
 

plugins/Facebook/FacebookPlugin.php
@@ -406,9 +406,9 @@ class FacebookPlugin extends Plugin
         $action_name = $action->trimmed('action');
 
         $action->menuItem(common_local_url('FBConnectLogin'),
-                                           _('Facebook'),
-                                           _('Login or register using Facebook'),
-                                             'FBConnectLogin' === $action_name);
+                                           _m('Facebook'),
+                                           _m('Login or register using Facebook'),
+                                           'FBConnectLogin' === $action_name);
 
         return true;
     }
@@ -426,8 +426,8 @@ class FacebookPlugin extends Plugin
         $action_name = $action->trimmed('action');
 
         $action->menuItem(common_local_url('FBConnectSettings'),
-                          _('Facebook'),
-                          _('Facebook Connect Settings'),
+                          _m('Facebook'),
+                          _m('Facebook Connect Settings'),
                           $action_name === 'FBConnectSettings');
 
         return true;

commit 2f59f0ddb5cc6030671af7e751e0ae85e4751497

Compat fix for PHP 5.2.4 -- drop unneeded new param to debug_backtrace(), caused error spew on older PHP (introduced PHP 5.2.5) Fix for regression in 4b5e977a7b1c390555d880d3dc7f8b8c6744646c

lib/language.php
@@ -153,7 +153,7 @@ if (!function_exists('npgettext')) {
  */
 function _m($msg/*, ...*/)
 {
-    $domain = _mdomain(debug_backtrace(false));
+    $domain = _mdomain(debug_backtrace());
     $args = func_get_args();
     switch(count($args)) {
     case 1: return dgettext($domain, $msg);