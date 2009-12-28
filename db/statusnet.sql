8d4e617d4e36fdcee7c2536085e5d2b13449e14a
Evan Prodromou <evan@status.net>
2009-12-28 13:53:28 -0800

    add table user_location_prefs

--- db/statusnet.sql
+++ db/statusnet.sql
@@ -587,3 +587,12 @@ create table login_token (
     constraint primary key (user_id)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
 
+create table user_location_prefs (
+    user_id integer not null comment 'user who has the preference' references user (id),
+    share_location tinyint default 1 comment 'Whether to share location data',
+    created datetime not null comment 'date this record was created',
+    modified timestamp comment 'date this record was modified',
+
+    constraint primary key (user_id)
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
+
