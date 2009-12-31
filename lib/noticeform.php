0320bf2fb3cdd4b9e6b635485421833fde447da8
Sarven Capadisli <csarven@status.net>
2009-12-31 15:46:43 +0000

    Use the location setting profile as secondary

--- lib/noticeform.php
+++ lib/noticeform.php
@@ -202,10 +202,10 @@ class NoticeForm extends Form
             $this->out->hidden('notice_in-reply-to', $this->inreplyto, 'inreplyto');
 
             if ($this->user->shareLocation()) {
-                $this->out->hidden('notice_data-lat', empty($this->profile->lat) ? null : $this->profile->lat, 'lat');
-                $this->out->hidden('notice_data-lon', empty($this->profile->lon) ? null : $this->profile->lon, 'lon');
-                $this->out->hidden('notice_data-location_id', empty($this->profile->location_id) ? null : $this->profile->location_id, 'location_id');
-                $this->out->hidden('notice_data-location_ns', empty($this->profile->location_ns) ? null : $this->profile->location_ns, 'location_ns');
+                $this->out->hidden('notice_data-lat', empty($this->lat) ? (empty($this->profile->lat) ? null : $this->profile->lat) : $this->lat, 'lat');
+                $this->out->hidden('notice_data-lon', empty($this->lon) ? (empty($this->profile->lon) ? null : $this->profile->lon) : $this->lon, 'lon');
+                $this->out->hidden('notice_data-location_id', empty($this->location_id) ? (empty($this->profile->location_id) ? null : $this->profile->location_id) : $this->location_id, 'location_id');
+                $this->out->hidden('notice_data-location_ns', empty($this->location_ns) ? (empty($this->profile->location_ns) ? null : $this->profile->location_ns) : $this->location_ns, 'location_ns');
 
                 $this->out->elementStart('div',array('id' => 'notice_data-location_enabled_container', 'data-geocode-url' => common_local_url('geocode')));
                 $this->out->checkbox('notice_data-location_enabled', _('Share your location'), true);
