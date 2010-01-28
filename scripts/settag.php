4a0413c0270bcac456c20972342c2c29182bec4e
Evan Prodromou <evan@status.net>
2010-01-28 01:42:52 -0500

    Add a script to set tags for sites

---
+++ scripts/settag.php
@@ -0,0 +1,84 @@
+#!/usr/bin/env php
+<?php
+/*
+ * StatusNet - a distributed open-source microblogging tool
+ * Copyright (C) 2008, 2009, StatusNet, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+define('INSTALLDIR', realpath(dirname(__FILE__) . '/..'));
+
+$shortoptions = 'd';
+$longoptions = array('delete');
+
+$helptext = <<<END_OF_SETTAG_HELP
+settag.php [options] <site> <tag>
+Set the tag <tag> for site <site>.
+
+With -d, delete the tag.
+
+END_OF_SETTAG_HELP;
+
+require_once INSTALLDIR.'/scripts/commandline.inc';
+
+if (count($args) != 2) {
+    show_help();
+    exit(1);
+}
+
+$nickname = $args[0];
+$tag = strtolower($args[1]);
+
+$sn = Status_network::memGet('nickname', $nickname);
+
+if (empty($sn)) {
+    print "No such site.\n";
+    exit(-1);
+}
+
+$tags = $sn->getTags();
+
+$i = array_search($tags, $tag);
+
+if ($i !== false) {
+    if (have_option('d', 'delete')) { // Delete
+        unset($tags[$i]);
+
+        $orig = clone($sn);
+        $sn->tags = implode('|', $tags);
+        $result = $sn->update($orig);
+        if (!$result) {
+            print "Couldn't update.\n";
+            exit(-1);
+        }
+    } else {
+        print "Already set.\n";
+        exit(-1);
+    }
+} else {
+    if (have_option('d', 'delete')) { // Delete
+        print "No such tag.\n";
+        exit(-1);
+    } else {
+        $tags[] = $tag;
+        $orig = clone($sn);
+        $sn->tags = implode('|', $tags);
+        $result = $sn->update($orig);
+        if (!$result) {
+            print "Couldn't update.\n";
+            exit(-1);
+        }
+    }
+}
