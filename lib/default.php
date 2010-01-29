440ee00b1ed9c1b0b552c14b72c962a79e1a402e
Zach Copley <zach@status.net>
2010-01-28 22:04:14 -0800

    Move sessions settings to its own panel

--- lib/default.php
+++ lib/default.php
@@ -266,7 +266,7 @@ $default =
                                  'OpenID' => null),
               ),
         'admin' =>
-        array('panels' => array('design', 'site', 'user', 'paths', 'access')),
+        array('panels' => array('design', 'site', 'user', 'paths', 'access', 'sessions')),
         'singleuser' =>
         array('enabled' => false,
               'nickname' => null),
