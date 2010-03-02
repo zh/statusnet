de65b15f9df6703268eeff92502152605ec0f536
Zach Copley <zach@status.net>
2010-03-01 21:34:50 -0800

    Initial Twitter bridge admin panel

--- plugins/TwitterBridge/twitterauthorization.php
+++ plugins/TwitterBridge/twitterauthorization.php
@@ -47,7 +47,7 @@ require_once INSTALLDIR . '/plugins/TwitterBridge/twitter.php';
  * @author   Zach Copley <zach@status.net>
  * @author   Julien C <chaumond@gmail.com>
  * @license  http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License version 3.0
- * @link     http://laconi.ca/
+ * @link     http://status.net/
  *
  */
 class TwitterauthorizationAction extends Action
