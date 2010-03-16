fa1262f51e95fb64d64105cf2a6cce77a227cd6c
Jeffery To <jeffery.to@gmail.com>
2010-03-16 17:31:05 +0800

    Fixed IE7 prompting the user to download OpenSearch description xml after login (for a private site)
    Flow:
    1. Browser (IE7) is redirected to the login page.
    2. Browser reads the page, sees OpenSearch descriptions, tries to
       download them. Each request gets recorded by SN as the page the user
       should be redirected to after logging in (returnto).
    3. User logs in, then gets redirected to the returnto action, which is
       an OpenSearch description.
    
    The OpenSearch descriptions aren't sensitive so making them public in a
    private site should be okay.
    
    (I recall fixing this in 0.8.x... :-( )

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ function checkMirror($action_obj, $args)
 
 function isLoginAction($action)
 {
-    static $loginActions =  array('login', 'recoverpassword', 'api', 'doc', 'register', 'publicxrds', 'otp');
+    static $loginActions =  array('login', 'recoverpassword', 'api', 'doc', 'register', 'publicxrds', 'otp', 'opensearch');
 
     $login = null;
 
