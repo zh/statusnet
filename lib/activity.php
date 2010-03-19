13b1acdd084289d1f1a0c02c51633bdd92b46cf1
Evan Prodromou <evan@status.net>
2010-03-19 15:28:25 -0500

    only search elements for links

--- lib/activity.php
+++ lib/activity.php
@@ -347,6 +347,11 @@ class ActivityUtils
         $els = $element->childNodes;
 
         foreach ($els as $link) {
+
+            if (!($link instanceof DOMElement)) {
+                continue;
+            }
+
             if ($link->localName == self::LINK && $link->namespaceURI == self::ATOM) {
 
                 $linkRel = $link->getAttribute(self::REL);
