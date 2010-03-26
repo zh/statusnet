4484c63ff4add940cf3fb6c25d41b5700beb38df
Sarven Capadisli <csarven@status.net>
2010-03-26 14:56:15 +0100

    Fix for XHR more text behaviour on conversation pages

--- js/util.js
+++ js/util.js
@@ -401,10 +401,11 @@ var SN = { // StatusNet
 
             var attachment_more = notice.find('.attachment.more');
             if (attachment_more.length > 0) {
-                attachment_more.click(function() {
-                    $(this).addClass(SN.C.S.Processing);
-                    $.get($(this).attr('href')+'/ajax', null, function(data) {
-                        notice.find('.entry-title .entry-content').html($(data).find('#attachment_view .entry-content').html());
+                $(attachment_more[0]).click(function() {
+                    var m = $(this);
+                    m.addClass(SN.C.S.Processing);
+                    $.get(m.attr('href')+'/ajax', null, function(data) {
+                        m.parent('.entry-content').html($(data).find('#attachment_view .entry-content').html());
                     });
 
                     return false;
