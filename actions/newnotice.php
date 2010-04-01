8b24ad8a9c681585e95612084eb629df8b364b74
Evan Prodromou <evan@status.net>
2010-04-01 12:52:12 -0400

    Revert "if user allows location sharing but turned off browser location use profile location"
    This reverts commit 4b80ce0be89fe50eabec1a19dbf4a0c26a413423.

--- actions/newnotice.php
+++ actions/newnotice.php
@@ -184,21 +184,13 @@ class NewnoticeAction extends Action
 
         $options = array('reply_to' => ($replyto == 'false') ? null : $replyto);
 
-        if ($user->shareLocation()) {
-            // use browser data if checked; otherwise profile data
-            if ($this->arg('notice_data-geo')) {
-                $locOptions = Notice::locationOptions($this->trimmed('lat'),
-                                                      $this->trimmed('lon'),
-                                                      $this->trimmed('location_id'),
-                                                      $this->trimmed('location_ns'),
-                                                      $user->getProfile());
-            } else {
-                $locOptions = Notice::locationOptions(null,
-                                                      null,
-                                                      null,
-                                                      null,
-                                                      $user->getProfile());
-            }
+        if ($user->shareLocation() && $this->arg('notice_data-geo')) {
+
+            $locOptions = Notice::locationOptions($this->trimmed('lat'),
+                                                  $this->trimmed('lon'),
+                                                  $this->trimmed('location_id'),
+                                                  $this->trimmed('location_ns'),
+                                                  $user->getProfile());
 
             $options = array_merge($options, $locOptions);
         }
@@ -208,6 +200,8 @@ class NewnoticeAction extends Action
         if (isset($upload)) {
             $upload->attachToNotice($notice);
         }
+
+
 
         if ($this->boolean('ajax')) {
             header('Content-Type: text/xml;charset=utf-8');
