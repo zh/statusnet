09208f8d654336d710069c1b4843de7e0d8c5d20
Brion Vibber <brion@pobox.com>
2010-04-21 17:16:42 +0200

    Basic custom CSS and theme uploading features. 'local' subdir can now be customized to a distinct directory and URL path to make it easier to separate custom themes for a multi-site farm running a common code base.
    Currently only one custom theme may be uploaded per site, saved with the name 'custom' and stored into the local/themes subdirectory.
    Administrators can upload a .ZIP archive containing a theme through the design admin panel; its contents are validated to ensure that only legit files are saved, and a 5M size quota is enforced.
    Theme upload requires the zip extension for PHP; if not present, theme uploading is disabled by default.
    
    Uploading and the custom CSS can be controlled via $config['theme_upload']['enabled'] and $config['custom_css']['enabled'].
    
    Configurable directory/path/server for 'local' subdirectory (currently only as used for themes; local plugins not yet switched over)
    
    Can set $config['local']['dir'] etc; not currently exposed in the admin panels.
    
    Per-site directories on a separate themes server could be set up such as:
      $config['local']['dir'] = '/path/to/themes/local/' . $_nickname;
      $config['local']['server'] = 'themes.example.com';
      $config['local']['path'] = '/local/' . $_nickname;
      $config['local']['ssl'] = 'never';

--- actions/designadminpanel.php
+++ actions/designadminpanel.php
@@ -125,9 +125,19 @@ class DesignadminpanelAction extends AdminPanelAction
             return;
         }
 
-        // check for an image upload
+        // check for file uploads
 
         $bgimage = $this->saveBackgroundImage();
+        $customTheme = $this->saveCustomTheme();
+
+        $oldtheme = common_config('site', 'theme');
+        if ($customTheme) {
+            // This feels pretty hacky :D
+            $this->args['theme'] = $customTheme;
+            $themeChanged = true;
+        } else {
+            $themeChanged = ($this->trimmed('theme') != $oldtheme);
+        }
 
         static $settings = array('theme', 'logo');
 
@@ -139,15 +149,13 @@ class DesignadminpanelAction extends AdminPanelAction
 
         $this->validate($values);
 
-        $oldtheme = common_config('site', 'theme');
-
         $config = new Config();
 
         $config->query('BEGIN');
 
         // Only update colors if the theme has not changed.
 
-        if ($oldtheme == $values['theme']) {
+        if (!$themeChanged) {
 
             $bgcolor = new WebColor($this->trimmed('design_background'));
             $ccolor  = new WebColor($this->trimmed('design_content'));
@@ -187,6 +195,13 @@ class DesignadminpanelAction extends AdminPanelAction
 
         if (isset($bgimage)) {
             Config::save('design', 'backgroundimage', $bgimage);
+        }
+
+        if (common_config('custom_css', 'enabled')) {
+            $css = $this->arg('css');
+            if ($css != common_config('custom_css', 'css')) {
+                Config::save('custom_css', 'css', $css);
+            }
         }
 
         $config->query('COMMIT');
@@ -259,6 +274,33 @@ class DesignadminpanelAction extends AdminPanelAction
             }
 
             return $filename;
+        }
+    }
+
+    /**
+     * Save the custom theme if the user uploaded one.
+     * 
+     * @return mixed custom theme name, if succesful, or null if no theme upload.
+     * @throws ClientException for invalid theme archives
+     * @throws ServerException if trouble saving the theme files
+     */
+
+    function saveCustomTheme()
+    {
+        if (common_config('theme_upload', 'enabled') &&
+            $_FILES['design_upload_theme']['error'] == UPLOAD_ERR_OK) {
+
+            $upload = ThemeUploader::fromUpload('design_upload_theme');
+            $basedir = common_config('local', 'dir');
+            if (empty($basedir)) {
+                $basedir = INSTALLDIR . '/local';
+            }
+            $name = 'custom'; // @todo allow multiples, custom naming?
+            $outdir = $basedir . '/theme/' . $name;
+            $upload->extract($outdir);
+            return $name;
+        } else {
+            return null;
         }
     }
 
@@ -374,6 +416,7 @@ class DesignAdminPanelForm extends AdminForm
         $this->showTheme();
         $this->showBackground();
         $this->showColors();
+        $this->showAdvanced();
     }
 
     function showLogo()
@@ -417,6 +460,16 @@ class DesignAdminPanelForm extends AdminForm
                              $themes, _('Theme for the site.'),
                              false, $this->value('theme'));
         $this->unli();
+
+        if (common_config('theme_upload', 'enabled')) {
+            $this->li();
+            $this->out->element('label', array('for' => 'design_upload_theme'), _('Custom theme'));
+            $this->out->element('input', array('id' => 'design_upload_theme',
+                                               'name' => 'design_upload_theme',
+                                               'type' => 'file'));
+            $this->out->element('p', 'form_guide', _('You can upload a custom StatusNet theme as a .ZIP archive.'));
+            $this->unli();
+        }
 
         $this->out->elementEnd('ul');
 
@@ -502,6 +555,8 @@ class DesignAdminPanelForm extends AdminForm
 
     function showColors()
     {
+        $design = $this->out->design;
+
         $this->out->elementStart('fieldset', array('id' => 'settings_design_color'));
         $this->out->element('legend', null, _('Change colours'));
 
@@ -584,6 +639,27 @@ class DesignAdminPanelForm extends AdminForm
         $this->out->elementEnd('fieldset');
 
         $this->out->elementEnd('ul');
+    }
+
+    function showAdvanced()
+    {
+        if (common_config('custom_css', 'enabled')) {
+            $this->out->elementStart('fieldset', array('id' => 'settings_design_advanced'));
+            $this->out->element('legend', null, _('Advanced'));
+            $this->out->elementStart('ul', 'form_data');
+
+            $this->li();
+            $this->out->element('label', array('for' => 'css'), _('Custom CSS'));
+            $this->out->element('textarea', array('name' => 'css',
+                                            'id' => 'css',
+                                            'cols' => '50',
+                                            'rows' => '10'),
+                                strval(common_config('custom_css', 'css')));
+            $this->unli();
+
+            $this->out->elementEnd('fieldset');
+            $this->out->elementEnd('ul');
+        }
     }
 
     /**
