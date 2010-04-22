809e597841d1337b641784eee21d5e9b5dc297e1
Craig Andrews <candrews@integralblue.com>
2010-04-22 17:57:57 -0400

    Only shorten after the user presses space, or following a paste operation

--- plugins/ClientSideShorten/shorten.js
+++ plugins/ClientSideShorten/shorten.js
@@ -1,46 +1,59 @@
-// smart(x) from Paul Irish
-// http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
+//wrap everything in a self-executing anonymous function to avoid conflicts
+(function(){
 
-(function($,sr){
+    // smart(x) from Paul Irish
+    // http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
 
-    // debouncing function from John Hann
-    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-    var debounce = function (func, threshold, execAsap) {
-        var timeout;
+    (function($,sr){
 
-        return function debounced () {
-            var obj = this, args = arguments;
-            function delayed () {
-                if (!execAsap)
+        // debouncing function from John Hann
+        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+        var debounce = function (func, threshold, execAsap) {
+            var timeout;
+
+            return function debounced () {
+                var obj = this, args = arguments;
+                function delayed () {
+                    if (!execAsap)
+                        func.apply(obj, args);
+                        timeout = null; 
+                };
+
+                if (timeout)
+                    clearTimeout(timeout);
+                else if (execAsap)
                     func.apply(obj, args);
-                    timeout = null; 
+
+                timeout = setTimeout(delayed, threshold || 100); 
             };
+        }
+        jQuery.fn[sr] = function(fn){  return fn ? this.bind('keypress', debounce(fn, 1000)) : this.trigger(sr); };
 
-            if (timeout)
-                clearTimeout(timeout);
-            else if (execAsap)
-                func.apply(obj, args);
+    })(jQuery,'smartkeypress');
 
-            timeout = setTimeout(delayed, threshold || 100); 
-        };
-    }
-    jQuery.fn[sr] = function(fn){  return fn ? this.bind('keypress', debounce(fn, 1000)) : this.trigger(sr); };
+    $(document).ready(function(){
+        $noticeDataText = $('#'+SN.C.S.NoticeDataText);
+        $noticeDataText.smartkeypress(function(e){
+            if(e.charCode == '32') {
+                shorten();
+            }
+        });
+        $noticeDataText.bind('paste', shorten);
+    });
 
-})(jQuery,'smartkeypress');
-
-$(document).ready(function(){
-    $('#notice_data-text').smartkeypress(function(e){  
-        var original = $('#notice_data-text').val();
+    function shorten()
+    {
+        $noticeDataText = $('#'+SN.C.S.NoticeDataText);
+        var original = $noticeDataText.val();
         $.ajax({
             url: $('address .url')[0].href+'/plugins/ClientSideShorten/shorten',
-            data: { text: $('#notice_data-text').val() },
+            data: { text: $noticeDataText.val() },
             dataType: 'text',
             success: function(data) {
-                if(original == $('#notice_data-text').val()) {
-                    $('#notice_data-text').val(data);
-                    $('#notice_data-text').keyup();
+                if(original == $noticeDataText.val()) {
+                    $noticeDataText.val(data).keyup();
                 }
             }
         });
-    });
-});
+    }
+})();
