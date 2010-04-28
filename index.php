f5c82d9d60afd044f9652865d3e481dec974075f
Brion Vibber <brion@pobox.com>
2010-04-28 11:05:31 -0700

    add rsd to the list of login actions
    Allows rsd.xml to be reached on private sites. Fixes http://status.net/trac/ticket/2309
    
    Conflicts:
    
    	index.php

--- index.php
+++ index.php
@@ -185,7 +185,7 @@ function checkMirror($action_obj, $args)
 
 function isLoginAction($action)
 {
-    static $loginActions =  array('login', 'recoverpassword', 'api', 'doc', 'register', 'publicxrds', 'otp');
+    static $loginActions =  array('login', 'recoverpassword', 'api', 'doc', 'register', 'publicxrds', 'otp', 'rsd');
 
     $login = null;
 
