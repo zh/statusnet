80516cba2f550f8f6a4243adb536f3b4aaab0b5f
Luke Fitzgerald <lw.fitzgerald@googlemail.com>
2010-07-09 02:43:40 -0700

    Extended Phergie_Config to allow passing config array instead of loading from file

---
+++ plugins/Irc/extlib/phergie/Phergie/ExtendedConfig.php
@@ -0,0 +1,26 @@
+<?php
+class Phergie_Extended_Config extends Phergie_Config {
+    /**
+     * Incorporates an associative array of settings into the current
+     * configuration settings.
+     *
+     * @param array $array Array of settings
+     *
+     * @return Phergie_Config Provides a fluent interface
+     * @throws Phergie_Config_Exception
+     */
+    public function readArray($array) {
+        $settings = $array;
+        if (!is_array($settings)) {
+            throw new Phergie_Config_Exception(
+                'Parameter is not an array',
+                Phergie_Config_Exception::ERR_ARRAY_NOT_RETURNED
+            );
+        }
+
+        $this->files[$file] = array_keys($settings);
+        $this->settings += $settings;
+
+        return $this;
+    }
+}
