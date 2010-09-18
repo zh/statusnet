16d234f9af51dfd4389232dc2d67e08a1f539232
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-09-18 23:49:49 +0200

    Remove superfluous whitespace

--- plugins/TwitterBridge/TwitterBridgePlugin.php
+++ plugins/TwitterBridge/TwitterBridgePlugin.php
@@ -47,7 +47,6 @@ require_once INSTALLDIR . '/plugins/TwitterBridge/twitter.php';
  */
 class TwitterBridgePlugin extends Plugin
 {
-
     const VERSION = STATUSNET_VERSION;
     public $adminImportControl = false; // Should the 'import' checkbox be exposed in the admin panel?
 
