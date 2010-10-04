82f05d0a61752d0552bc8029b2a55ab7c5171b33
Zach Copley <zach@status.net>
2010-10-04 16:47:20 -0700

    Somewhat improved test script for fetching an OAuth request token

--- tests/oauth/getrequesttoken.php
+++ tests/oauth/getrequesttoken.php
@@ -24,47 +24,54 @@ require_once INSTALLDIR . '/scripts/commandline.inc';
 require_once INSTALLDIR . '/extlib/OAuth.php';
 
 $ini = parse_ini_file("oauth.ini");
-
 $test_consumer = new OAuthConsumer($ini['consumer_key'], $ini['consumer_secret']);
-
 $rt_endpoint = $ini['apiroot'] . $ini['request_token_url'];
-
 $parsed = parse_url($rt_endpoint);
 $params = array();
-
 parse_str($parsed['query'], $params);
 
 $hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
 
-$req_req = OAuthRequest::from_consumer_and_token($test_consumer, NULL, "GET", $rt_endpoint, $params);
-$req_req->sign_request($hmac_method, $test_consumer, NULL);
-
-$r = httpRequest($req_req->to_url());
+try {
+    $req_req = OAuthRequest::from_consumer_and_token($test_consumer, NULL, "GET", $rt_endpoint, $params);
+    $req_req->sign_request($hmac_method, $test_consumer, NULL);
+    $r = httpRequest($req_req->to_url());
+} catch (Exception $e) {
+    print $e->getMessage();
+    var_dump($req_req);
+    exit(1);
+}
 
 $body = $r->getBody();
-
 $token_stuff = array();
 parse_str($body, $token_stuff);
 
-$authurl = $ini['apiroot'] . $ini['authorize_url'] . '?oauth_token=' . $token_stuff['oauth_token'];
+if (empty($token_stuff['oauth_token'])) {
+    print "Error: $body\n";
+    exit(1);
+}
 
+$authurl = $ini['apiroot'] . $ini['authorize_url'] . '?oauth_token=' . $token_stuff['oauth_token'];
+print "\nSuccess!\n\n";
 print 'Request token        : ' . $token_stuff['oauth_token'] . "\n";
 print 'Request token secret : ' . $token_stuff['oauth_token_secret'] . "\n";
 print "Authorize URL        : $authurl\n";
 
-//var_dump($req_req);
+print "\nNow paste the Authorize URL into your browser and authorize the request token.\n";
 
 function httpRequest($url)
 {
     $request = HTTPClient::start();
     
-    $request->setConfig(array(
-			      'follow_redirects' => true,
-			      'connect_timeout' => 120,
-			      'timeout' => 120,
-			      'ssl_verify_peer' => false,
-			      'ssl_verify_host' => false
-			      ));
+    $request->setConfig(
+        array(
+            'follow_redirects' => true,
+	    'connect_timeout' => 120,
+	    'timeout' => 120,
+	    'ssl_verify_peer' => false,
+	    'ssl_verify_host' => false
+        )
+    );
     
     return $request->get($url);
 }
