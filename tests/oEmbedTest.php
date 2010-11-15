68ff57f230c35a7b8ecfcb5c95f219b14f21f60f
Brion Vibber <brion@pobox.com>
2010-11-15 12:25:44 -0800

    Restructure oembed test sources

--- tests/oEmbedTest.php
+++ tests/oEmbedTest.php
@@ -24,7 +24,7 @@ class oEmbedTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider fileTypeCases
+     * @dataProvider fallbackSources
      *
      */
     public function testoEmbed($url, $expectedType)
@@ -41,27 +41,51 @@ class oEmbedTest extends PHPUnit_Framework_TestCase
         }
     }
 
-    static public function fileTypeCases()
+    /**
+     * Sample oEmbed targets for sites we know ourselves...
+     * @return array
+     */
+    static public function knownSources()
     {
-        $files = array(
-            'http://www.flickr.com/photos/brionv/5172500179/' => 'photo',
-            'http://twitpic.com/36adw6' => 'photo',
-            'http://yfrog.com/fy42747177j' => 'photo',
-            'http://identi.ca/attachment/34437400' => 'photo',
-
-            'http://www.youtube.com/watch?v=eUgLR232Cnw' => 'video',
-            'http://vimeo.com/9283184' => 'video',
-
-            'http://en.wikipedia.org/wiki/File:Wiki.png' => 'link', // @fixme in future there may be a native provider -- will change to 'photo'
-            'http://leuksman.com/log/2010/10/29/statusnet-0-9-6-release/' => 'none',
+        $sources = array(
+            array('http://www.flickr.com/photos/brionv/5172500179/', 'photo'),
+            array('http://yfrog.com/fy42747177j', 'photo'),
+            array('http://twitpic.com/36adw6', 'photo'),
         );
-
-        $dataset = array();
-        foreach ($files as $url => $type) {
-            $dataset[] = array($url, $type);
-        }
-        return $dataset;
+        return $sources;
     }
 
-}
+    /**
+     * Sample oEmbed targets that can be found via discovery.
+     * Includes also knownSources() output.
+     *
+     * @return array
+     */
+    static public function discoverableSources()
+    {
+        $sources = array(
+            array('http://identi.ca/attachment/34437400', 'photo'),
 
+            array('http://www.youtube.com/watch?v=eUgLR232Cnw', 'video'),
+            array('http://vimeo.com/9283184', 'video'),
+
+            // Will fail discovery:
+            array('http://leuksman.com/log/2010/10/29/statusnet-0-9-6-release/', 'none'),
+        );
+        return array_merge(self::knownSources(), $sources);
+    }
+
+    /**
+     * Sample oEmbed targets that can be found via oohembed.com.
+     * Includes also discoverableSources() output.
+     *
+     * @return array
+     */
+    static public function fallbackSources()
+    {
+        $sources = array(
+            array('http://en.wikipedia.org/wiki/File:Wiki.png', 'link'), // @fixme in future there may be a native provider -- will change to 'photo'
+        );
+        return array_merge(self::discoverableSources(), $sources);
+    }
+}
