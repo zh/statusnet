da4f8d465ffdf9833c495699eff966810b522c5d
Evan Prodromou <evan@status.net>
2010-11-17 12:16:50 -0500

    Use minified version of util.js

--- lib/action.php
+++ lib/action.php
@@ -282,7 +282,7 @@ class Action extends HTMLOutputter // lawsuit
             }
             if (Event::handle('StartShowStatusNetScripts', array($this)) &&
                 Event::handle('StartShowLaconicaScripts', array($this))) {
-                $this->script('util.js');
+                $this->script('util.min.js');
                 $this->showScriptMessages();
                 // Frame-busting code to avoid clickjacking attacks.
                 $this->inlineScript('if (window.top !== window.self) { window.top.location.href = window.self.location.href; }');
