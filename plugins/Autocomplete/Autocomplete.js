daae13df1c2d202cba0769803a9ef03892495ccb
Brion Vibber <brion@pobox.com>
2010-12-06 16:20:54 -0800

    Start tweaking Autocomplete js

--- plugins/Autocomplete/Autocomplete.js
+++ plugins/Autocomplete/Autocomplete.js
@@ -1,27 +1,22 @@
 $(document).ready(function(){
+    function fullName(row) {
+        if (typeof row.fullname == "string" && row.fullname != '') {
+            return row.nickname + ' (' + row.fullname + ')';
+        } else {
+            return row.nickname;
+        }
+    }
             $('#notice_data-text').autocomplete($('address .url')[0].href+'/plugins/Autocomplete/autocomplete.json', {
                 multiple: true,
                 multipleSeparator: " ",
                 minChars: 1,
                 formatItem: function(row, i, max){
                     row = eval("(" + row + ")");
-                    switch(row.type)
-                    {
-                        case 'user':
-                            return row.nickname + ' (' + row.fullname + ')';
-                        case 'group':
-                            return row.nickname + ' (' + row.fullname + ')';
-                    }
+                    return fullName(row);
                 },
                 formatMatch: function(row, i, max){
                     row = eval("(" + row + ")");
-                    switch(row.type)
-                    {
-                        case 'user':
-                            return row.nickname;
-                        case 'group':
-                            return row.nickname;
-                    }
+                    return row.nickname;
                 },
                 formatResult: function(row){
                     row = eval("(" + row + ")");
