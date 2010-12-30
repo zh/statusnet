b00a3cd4e4b279b075e7786d850f63dacdd9ee98
Brion Vibber <brion@pobox.com>
2010-12-30 13:37:05 -0800

    Make sure the UUIDs are unique too :)

--- tests/UUIDTest.php
+++ tests/UUIDTest.php
@@ -21,5 +21,17 @@ class UUIDTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(0x4000, hexdec(substr($result, 14, 4)) & 0xF000);
         $this->assertEquals(0x8000, hexdec(substr($result, 19, 4)) & 0xC000);
     }
+
+    public function testUnique()
+    {
+        $reps = 100;
+        $ids = array();
+
+        for ($i = 0; $i < $reps; $i++) {
+            $ids[] = UUID::gen();
+        }
+
+        $this->assertEquals(count($ids), count(array_unique($ids)), "UUIDs must be unique");
+    }
 }
 
