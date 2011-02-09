dc5daa237e8d691ca9be85da3ab883b00cee74e1
Brion Vibber <brion@pobox.com>
2011-02-09 00:09:25 -0800

    Further tweak for ssl paths in plugin check (sslserver may be set but empty)

--- lib/plugin.php
+++ lib/plugin.php
@@ -143,18 +143,18 @@ class Plugin
             $server = common_config('plugins', 'server');
         }
 
-        if (is_null($server)) {
+        if (empty($server)) {
             if ($isHTTPS) {
                 $server = common_config('site', 'sslserver');
             }
-            if (is_null($server)) {
+            if (empty($server)) {
                 $server = common_config('site', 'server');
             }
         }
 
         $path = common_config('plugins', 'path');
 
-        if (is_null($path)) {
+        if (empty($path)) {
             $path = common_config('site', 'path') . '/plugins/';
         }
 
