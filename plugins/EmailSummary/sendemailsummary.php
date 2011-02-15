f31db669b0fed67a5a49415dcbbcbd5d9766a70b
Evan Prodromou <evan@status.net>
2011-02-15 16:27:51 -0500

    fix indentation in sendemailsummary.php

--- plugins/EmailSummary/sendemailsummary.php
+++ plugins/EmailSummary/sendemailsummary.php
@@ -1,22 +1,22 @@
 #!/usr/bin/env php
-<?php
-/*
- * StatusNet - a distributed open-source microblogging tool
- * Copyright (C) 2010, StatusNet, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   <?php
+   /*
+    * StatusNet - a distributed open-source microblogging tool
+    * Copyright (C) 2010, StatusNet, Inc.
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Affero General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+    *
+    * You should have received a copy of the GNU Affero General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
 
 define('INSTALLDIR', realpath(dirname(__FILE__) . '/../..'));
 
@@ -27,9 +27,9 @@ $helptext = <<<END_OF_SENDEMAILSUMMARY_HELP
 sendemailsummary.php [options]
 Send an email summary of the inbox to users
 
-  -i --id       ID of user to send summary to
-  -n --nickname nickname of the user to send summary to
-  -a --all      send summary to all users
+ -i --id       ID of user to send summary to
+ -n --nickname nickname of the user to send summary to
+ -a --all      send summary to all users
 
 END_OF_SENDEMAILSUMMARY_HELP;
 
@@ -45,3 +45,4 @@ try {
 } catch (NoUserArgumentException $nuae) {
     $qm->enqueue(null, 'sitesum');
 }
+
