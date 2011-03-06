52952d13c0b926b568d537d837aa36fcfcd9afa0
Evan Prodromou <evan@status.net>
2011-03-06 16:33:36 -0500

    first (non-working) move to microapp structure for bookmarks

--- lib/microappplugin.php
+++ lib/microappplugin.php
@@ -51,11 +51,14 @@ if (!defined('STATUSNET')) {
 
 abstract class MicroAppPlugin extends Plugin
 {
+    abstract function appTitle();
+    abstract function tag();
     abstract function types();
-    abstract function saveNoticeFromActivity($activity);
-    abstract function activityFromNotice($notice);
+    abstract function saveNoticeFromActivity($activity, $actor);
+    abstract function activityObjectFromNotice($notice);
     abstract function showNotice($notice, $out);
-    abstract function entryForm();
+    abstract function entryForm($out);
+    abstract function deleteRelated($notice);
 
     function isMyNotice($notice) {
         $types = $this->types();
@@ -66,5 +69,210 @@ abstract class MicroAppPlugin extends Plugin
         $types = $this->types();
         return (count($activity->objects) == 1 &&
                 in_array($activity->objects[0]->type, $types));
+    }
+
+    /**
+     * When a notice is deleted, delete the related objects
+     *
+     * @param Notice $notice Notice being deleted
+     * 
+     * @return boolean hook value
+     */
+
+    function onNoticeDeleteRelated($notice)
+    {
+        if ($this->isMyNotice($notice)) {
+            $this->deleteRelated($notice);
+        }
+
+        return true;
+    }
+
+    /**
+     * Output the HTML for this kind of object in a list
+     *
+     * @param NoticeListItem $nli The list item being shown.
+     *
+     * @return boolean hook value
+     */
+
+    function onStartShowNoticeItem($nli)
+    {
+        if (!$this->isMyNotice($nli->notice)) {
+            return true;
+        }
+
+        $out = $nli->out;
+
+        $this->showNotice($notice, $out);
+
+        $nli->showNoticeLink();
+        $nli->showNoticeSource();
+        $nli->showNoticeLocation();
+        $nli->showContext();
+        $nli->showRepeat();
+        
+        $out->elementEnd('div');
+        
+        $nli->showNoticeOptions();
+
+        return false;
+    }
+
+    /**
+     * Render a notice as one of our objects
+     *
+     * @param Notice         $notice  Notice to render
+     * @param ActivityObject &$object Empty object to fill
+     *
+     * @return boolean hook value
+     */
+     
+    function onStartActivityObjectFromNotice($notice, &$object)
+    {
+        if ($this->isMyNotice($notice)) {
+            $object = $this->activityObjectFromNotice($notice);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle a posted object from PuSH
+     *
+     * @param Activity        $activity activity to handle
+     * @param Ostatus_profile $oprofile Profile for the feed
+     *
+     * @return boolean hook value
+     */
+
+    function onStartHandleFeedEntryWithProfile($activity, $oprofile)
+    {
+        if ($this->isMyActivity($activity)) {
+
+            $actor = $oprofile->checkAuthorship($activity);
+
+            if (empty($actor)) {
+                throw new ClientException(_('Can\'t get author for activity.'));
+            }
+
+            $this->saveNoticeFromActivity($activity, $actor);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle a posted object from Salmon
+     *
+     * @param Activity $activity activity to handle
+     * @param mixed    $target   user or group targeted
+     *
+     * @return boolean hook value
+     */
+
+    function onStartHandleSalmonTarget($activity, $target)
+    {
+        if ($this->isMyActivity($activity)) {
+
+            $this->log(LOG_INFO, "Checking {$activity->id} as a valid Salmon slap.");
+
+            if ($target instanceof User_group) {
+                $uri = $target->getUri();
+                if (!in_array($uri, $activity->context->attention)) {
+                    throw new ClientException(_("Bookmark not posted ".
+                                                "to this group."));
+                }
+            } else if ($target instanceof User) {
+                $uri      = $target->uri;
+                $original = null;
+                if (!empty($activity->context->replyToID)) {
+                    $original = Notice::staticGet('uri', 
+                                                  $activity->context->replyToID); 
+                }
+                if (!in_array($uri, $activity->context->attention) &&
+                    (empty($original) ||
+                     $original->profile_id != $target->id)) {
+                    throw new ClientException(_("Bookmark not posted ".
+                                                "to this user."));
+                }
+            } else {
+                throw new ServerException(_("Don't know how to handle ".
+                                            "this kind of target."));
+            }
+
+            $actor = Ostatus_profile::ensureActivityObjectProfile($activity->actor);
+
+            $this->saveNoticeFromActivity($activity, $actor);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle object posted via AtomPub
+     *
+     * @param Activity &$activity Activity that was posted
+     * @param User     $user      User that posted it
+     * @param Notice   &$notice   Resulting notice
+     *
+     * @return boolean hook value
+     */
+
+    function onStartAtomPubNewActivity(&$activity, $user, &$notice)
+    {
+        if ($this->isMyActivity($activity)) {
+
+            $options = array('source' => 'atompub');
+
+            $this->saveNoticeFromActivity($activity,
+                                          $user->getProfile(),
+                                          $options);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handle object imported from a backup file
+     *
+     * @param User           $user     User to import for
+     * @param ActivityObject $author   Original author per import file
+     * @param Activity       $activity Activity to import
+     * @param boolean        $trusted  Is this a trusted user?
+     * @param boolean        &$done    Is this done (success or unrecoverable error)
+     *
+     * @return boolean hook value
+     */
+
+    function onStartImportActivity($user, $author, $activity, $trusted, &$done)
+    {
+        if ($this->isMyActivity($activity)) {
+
+            $obj = $activity->objects[0];
+
+            $options = array('uri' => $bookmark->id,
+                             'url' => $bookmark->link,
+                             'source' => 'restore');
+
+            $saved = $this->saveNoticeFromActivity($activity,
+                                                   $user->getProfile(),
+                                                   $options);
+
+            if (!empty($saved)) {
+                $done = true;
+            }
+
+            return false;
+        }
+
+        return true;
     }
 }
