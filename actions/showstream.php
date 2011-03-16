0bf40bd74807cdb1a62a6d7f073fe4bf83edf6fb
Brion Vibber <brion@pobox.com>
2011-03-16 14:54:14 -0700

    Output avatar/author along with ProfileNoticeListItem items on ShowProfileAction -- they get hidden in CSS anyway, and we need the author data for the inline reply form if you end up replying to yourself/someone else from a profile stream view.
    Note that the other modification -- of the repeat line -- is still going to be inconsistent between things originally on the page and anything that comes over Realtime.

--- actions/showstream.php
+++ actions/showstream.php
@@ -279,6 +279,18 @@ class ShowstreamAction extends ProfileAction
 
 // We don't show the author for a profile, since we already know who it is!
 
+/**
+ * Slightly modified from standard list; the author & avatar are hidden
+ * in CSS. We used to remove them here too, but as it turns out that
+ * confuses the inline reply code... and we hide them in CSS anyway
+ * since realtime updates come through in original form.
+ *
+ * Remaining customization right now is for the repeat marker, where
+ * it'll list who the original poster was instead of who did the repeat
+ * (since the repeater is you, and the repeatee isn't shown!)
+ * This will remain inconsistent if realtime updates come through,
+ * since those'll get rendered as a regular NoticeListItem.
+ */
 class ProfileNoticeList extends NoticeList
 {
     function newListItem($notice)
@@ -289,11 +301,6 @@ class ProfileNoticeList extends NoticeList
 
 class ProfileNoticeListItem extends DoFollowListItem
 {
-    function showAuthor()
-    {
-        return;
-    }
-
     /**
      * show a link to the author of repeat
      *
