e6afd16c02335ea7ca7541d9d2006af09bad7ebf
Brion Vibber <brion@pobox.com>
2011-03-18 16:35:50 -0700

    ModPlus plugin -- Ticket #3095: add remote profile options popup to group membership and other profile lists
    Popup remote profile options menu now shown on profile lists as well as notice lists.

--- plugins/ModPlus/modplus.js
+++ plugins/ModPlus/modplus.js
@@ -4,6 +4,7 @@
  */
 
 $(function() {
+    // Notice lists...
     $('.notice .author').live('mouseenter', function(e) {
         var notice = $(this).closest('.notice');
         var popup = notice.find('.remote-profile-options');
@@ -18,4 +19,21 @@ $(function() {
             popup.fadeOut();
         }
     });
+
+    // Profile lists...
+    $('.profile .avatar').live('mouseenter', function(e) {
+        var profile = $(this).closest('.profile');
+        var popup = profile.find('.remote-profile-options');
+        if (popup.length) {
+            popup.fadeIn();
+        }
+    });
+    $('.profile').live('mouseleave', function(e) {
+        var profile = $(this);
+        var popup = profile.find('.remote-profile-options');
+        if (popup.length) {
+            popup.fadeOut();
+        }
+    });
+
 });
