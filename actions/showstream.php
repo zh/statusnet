4292fa1fc4ed466a55d8129d87db475817cef044
Siebrand Mazeland <s.mazeland@xs4all.nl>
2011-03-31 22:43:57 +0200

    Tweak page titles. Thanks @evan for thinking with me.

--- actions/showstream.php
+++ actions/showstream.php
@@ -67,11 +67,11 @@ class ShowstreamAction extends ProfileAction
             if ($this->page == 1) {
                 // TRANS: Page title showing tagged notices in one user's stream.
                 // TRANS: %1$s is the username, %2$s is the hash tag.
-                return sprintf(_('%1$s tagged %2$s'), $base, $this->tag);
+                return sprintf(_('Notices by %1$s tagged %2$s'), $base, $this->tag);
             } else {
                 // TRANS: Page title showing tagged notices in one user's stream.
                 // TRANS: %1$s is the username, %2$s is the hash tag, %3$d is the page number.
-                return sprintf(_('%1$s tagged %2$s, page %3$d'), $base, $this->tag, $this->page);
+                return sprintf(_('Notices by %1$s tagged %2$s, page %3$d'), $base, $this->tag, $this->page);
             }
         } else {
             if ($this->page == 1) {
@@ -79,7 +79,7 @@ class ShowstreamAction extends ProfileAction
             } else {
                 // TRANS: Extended page title showing tagged notices in one user's stream.
                 // TRANS: %1$s is the username, %2$d is the page number.
-                return sprintf(_('%1$s, page %2$d'),
+                return sprintf(_('Notices by %1$s, page %2$d'),
                                $base,
                                $this->page);
             }
