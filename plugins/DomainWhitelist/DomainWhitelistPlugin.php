2019d32b3d62d12b460018c5d1f5f3e171b104b9
Evan Prodromou <evan@status.net>
2011-04-18 10:46:40 -0400

    let other validity checks go through first

--- plugins/DomainWhitelist/DomainWhitelistPlugin.php
+++ plugins/DomainWhitelist/DomainWhitelistPlugin.php
@@ -52,18 +52,20 @@ class DomainWhitelistPlugin extends Plugin
         return true;
     }
 
-    function onStartValidateUserEmail($user, $email, &$valid)
+    function onEndValidateUserEmail($user, $email, &$valid)
     {
-        if (!$this->matchesWhitelist($email)) {
-            $whitelist = $this->getWhitelist();
-            if (count($whitelist) == 1) {
-                $message = sprintf(_("Email address must be in this domain: %s"),
-                                   $whitelist[0]);
-            } else {
-                $message = sprintf(_("Email address must be in one of these domains: %s"),
-                                   implode(', ', $whitelist));
+        if ($valid) { // it's otherwise valid
+            if (!$this->matchesWhitelist($email)) {
+                $whitelist = $this->getWhitelist();
+                if (count($whitelist) == 1) {
+                    $message = sprintf(_("Email address must be in this domain: %s"),
+                                       $whitelist[0]);
+                } else {
+                    $message = sprintf(_("Email address must be in one of these domains: %s"),
+                                       implode(', ', $whitelist));
+                }
+                throw new ClientException($message);
             }
-            throw new ClientException($message);
         }
         return true;
     }
