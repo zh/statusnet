f3e0cd0b30d880b5d7f8cb9ab0af524d07c5bc7e
Zach Copley <zach@status.net>
2011-04-17 18:37:51 -0700

    Some special .js for the bookmark plugin, so we can control the form in the second step

---
+++ plugins/Bookmark/js/bookmark.js
@@ -0,0 +1,37 @@
+var Bookmark = {
+
+    // Special XHR that sends in some code to be run
+    // when the full bookmark form gets loaded
+    BookmarkXHR: function(form)
+    {
+        SN.U.FormXHR(form, Bookmark.InitBookmarkForm);
+        return false;
+    },
+
+    // Special initialization function just for the
+    // second step in the bookmarking workflow
+    InitBookmarkForm: function() {
+        alert('Gar!');
+
+        // Put fancy stuff here
+
+    }
+}
+
+$(document).ready(function() {
+
+    // Stop normal live event stuff
+    $('form.ajax').die();
+    $('form.ajax input[type=submit]').die();
+
+    // Make the bookmark submit super special
+    $('#form_initial_bookmark').bind('submit', function(e) {
+        Bookmark.BookmarkXHR($(this));
+        e.stopPropagation();
+        return false;
+    });
+
+    // Restore live event stuff to other forms & submit buttions
+    SN.Init.AjaxForms();
+
+});
